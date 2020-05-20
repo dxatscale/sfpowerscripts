2071dfb9be5a3aceabdad33e3fa79d7db748d983
azlamsalam <azlam.salam@outlook.com>
2020-05-20 14:46:30 +1000

    fix: promote to support new sfpowerscripts metadata

--- packages/azpipelines/BuildTasks/PromoteUnlockedPackageTask/PromoteUnlockedPackage.ts
+++ packages/azpipelines/BuildTasks/PromoteUnlockedPackageTask/PromoteUnlockedPackage.ts
@@ -4,15 +4,13 @@ var fs = require("fs");
 const path = require("path");
 import { AppInsights } from "../Common/AppInsights";
 
-
 async function run() {
   try {
-    
-
     console.log(`SFPowerScript.. Promote Unlocked Package`);
 
-
     const package_installedfrom = tl.getInput("packagepromotedfrom", true);
+    const sfdx_package: string = tl.getInput("package", true);
+    let devhub_alias = tl.getInput("devhub_alias", true);
 
     let package_version_id;
 
@@ -23,12 +21,27 @@ async function run() {
 
       let artifact_directory = tl.getVariable("system.artifactsDirectory");
 
-      let package_version_id_file_path = path.join(
+      //Newer metadata filename
+      let package_version_id_file_path;
+
+      package_version_id_file_path = path.join(
         artifact_directory,
         artifact,
         "sfpowerkit_artifact",
-        "artifact_metadata"
+        `${sfdx_package}_artifact_metadata`
       );
+
+      console.log(`Checking for directory ${package_version_id_file_path}`);
+
+      //Fallback to older format
+      if (!fs.existsSync(package_version_id_file_path)) {
+        package_version_id_file_path = path.join(
+          artifact_directory,
+          artifact,
+          "sfpowerkit_artifact",
+          "artifact_metadata"
+        );
+      }
 
       let package_metadata_json = fs
         .readFileSync(package_version_id_file_path)
@@ -36,18 +49,14 @@ async function run() {
 
       let package_metadata = JSON.parse(package_metadata_json);
 
-      package_version_id = package_metadata.package_version_id
-
+      package_version_id = package_metadata.package_version_id;
 
       console.log(`Found Package Version Id in artifact ${package_version_id}`);
-
-
     } else {
       package_version_id = tl.getInput("package_version_id", false);
     }
 
-
-    let devhub_alias = tl.getInput("devhub_alias", true);
+ 
 
     let promoteUnlockedPackageImpl: PromoteUnlockedPackageImpl = new PromoteUnlockedPackageImpl(
       package_version_id,
@@ -56,12 +65,16 @@ async function run() {
 
     await promoteUnlockedPackageImpl.exec();
 
-
     AppInsights.trackTask("sfpwowerscript-promoteunlockedpackage-task");
-    AppInsights.trackTaskEvent("sfpwowerscript-promoteunlockedpackage-task","package_oromoted");    
-
+    AppInsights.trackTaskEvent(
+      "sfpwowerscript-promoteunlockedpackage-task",
+      "package_oromoted"
+    );
   } catch (err) {
-    AppInsights.trackExcepiton("sfpwowerscript-promoteunlockedpackage-task",err);   
+    AppInsights.trackExcepiton(
+      "sfpwowerscript-promoteunlockedpackage-task",
+      err
+    );
     tl.setResult(tl.TaskResult.Failed, err.message);
   }
 }
