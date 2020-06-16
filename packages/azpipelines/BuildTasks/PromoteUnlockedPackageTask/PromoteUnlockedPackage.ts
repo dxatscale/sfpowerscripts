9b6fa1ab3adcc3d38ab92bd2f7cfa2c9cd9c080d
azlam.abdulsalam <azlam.abdulsalam@accenture.com>
2020-06-16 15:42:03 +1000

    add support for azure aritfacts / skip on missing artifacts

--- packages/azpipelines/BuildTasks/PromoteUnlockedPackageTask/PromoteUnlockedPackage.ts
+++ packages/azpipelines/BuildTasks/PromoteUnlockedPackageTask/PromoteUnlockedPackage.ts
@@ -3,74 +3,131 @@ import PromoteUnlockedPackageImpl from "@dxatscale/sfpowerscripts.core/lib/sfdxw
 var fs = require("fs");
 const path = require("path");
 
-
 async function run() {
   try {
+    
     console.log(`SFPowerScript.. Promote Unlocked Package`);
 
     const package_installedfrom = tl.getInput("packagepromotedfrom", true);
     const sfdx_package: string = tl.getInput("package", true);
-    let devhub_alias = tl.getInput("devhub_alias", true);
+    const devhub_alias = tl.getInput("devhub_alias", true);
     const projectDirectory = tl.getInput("project_directory", false);
-
-
+    const artifact = tl.getInput("artifact", true);
+    const skip_on_missing_artifact = tl.getBoolInput("skip_on_missing_artifact", false);
 
     let package_version_id;
 
-    if (package_installedfrom == "BuildArtifact") {
-      //Figure out the id from the artifact
-
-      const artifact = tl.getInput("artifact", true);
-
-      let artifact_directory = tl.getVariable("system.artifactsDirectory");
-
-      //Newer metadata filename
+    if (package_installedfrom == "Custom") {
+      package_version_id = tl.getInput("package_version_id", false);
+    } else {
       let package_version_id_file_path;
 
-      package_version_id_file_path = path.join(
-        artifact_directory,
-        artifact,
-        "sfpowerkit_artifact",
-        `${sfdx_package}_artifact_metadata`
-      );
-
-      console.log(`Checking for directory ${package_version_id_file_path}`);
-
-      //Fallback to older format
-      if (!fs.existsSync(package_version_id_file_path)) {
-        package_version_id_file_path = path.join(
-          artifact_directory,
-          artifact,
-          "sfpowerkit_artifact",
-          "artifact_metadata"
+      if (package_installedfrom == "BuildArtifact")
+        package_version_id_file_path = fetchArtifactFilePathFromBuildArtifact(
+          sfdx_package,
+          artifact
         );
-      }
+      else if (package_installedfrom == "AzureArtifact")
+        package_version_id_file_path = fetchArtifactFilePathFromAzureArtifact(
+          sfdx_package,
+          artifact
+        );
 
-      let package_metadata_json = fs
-        .readFileSync(package_version_id_file_path)
-        .toString();
-
-      let package_metadata = JSON.parse(package_metadata_json);
-
-      package_version_id = package_metadata.package_version_id;
-
-      console.log(`Found Package Version Id in artifact ${package_version_id}`);
-    } else {
-      package_version_id = tl.getInput("package_version_id", false);
+      missingArtifactDecider(
+        package_version_id_file_path,
+        skip_on_missing_artifact
+      );
     }
 
- 
-
-    let promoteUnlockedPackageImpl: PromoteUnlockedPackageImpl = new PromoteUnlockedPackageImpl(projectDirectory,
+    let promoteUnlockedPackageImpl: PromoteUnlockedPackageImpl = new PromoteUnlockedPackageImpl(
+      projectDirectory,
       package_version_id,
       devhub_alias
     );
 
     await promoteUnlockedPackageImpl.exec();
-
-   
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message);
+  }
+}
+
+function fetchArtifactFilePathFromBuildArtifact(
+  sfdx_package: string,
+  artifact: string
+): string {
+  let artifact_directory = tl.getVariable("system.artifactsDirectory");
+
+  //Newer metadata filename
+  let package_version_id_file_path = path.join(
+    artifact_directory,
+    artifact,
+    "sfpowerkit_artifact",
+    `${sfdx_package}_artifact_metadata`
+  );
+
+  console.log(
+    `Checking for ${sfdx_package} Build Artifact at path ${package_version_id_file_path}`
+  );
+  if (!fs.existsSync(package_version_id_file_path)) {
+    console.log(
+      `New Artifact format not found at the location ${package_version_id_file_path} `
+    );
+
+    console.log("Falling back to older artifact format");
+    package_version_id_file_path = path.join(
+      artifact_directory,
+      artifact,
+      "sfpowerkit_artifact",
+      `artifact_metadata`
+    );
+  }
+
+  return package_version_id_file_path;
+}
+
+function fetchArtifactFilePathFromAzureArtifact(
+  sfdx_package: string,
+  artifact: string
+): string {
+  let artifact_directory = tl.getVariable("system.artifactsDirectory");
+
+  //Newer metadata filename
+  let package_version_id_file_path = path.join(
+    artifact_directory,
+    artifact,
+    `${sfdx_package}_artifact_metadata`
+  );
+
+  console.log(
+    `Checking for ${sfdx_package} Azure Artifact at path ${package_version_id_file_path}`
+  );
+
+  return package_version_id_file_path;
+}
+
+function missingArtifactDecider(
+  package_version_id_file_path: string,
+  skip_on_missing_artifact: boolean
+): void {
+  if (
+    !fs.existsSync(package_version_id_file_path) &&
+    !skip_on_missing_artifact
+  ) {
+    throw new Error(
+      `Artifact not found at ${package_version_id_file_path}.. Please check the inputs`
+    );
+  } else if (
+    !fs.existsSync(package_version_id_file_path) &&
+    skip_on_missing_artifact
+  ) {
+    console.log(
+      `Skipping task as artifact is missing, and 'Skip If no artifact is found' ${skip_on_missing_artifact}`
+    );
+    tl.setResult(
+      tl.TaskResult.Skipped,
+      `Skipping task as artifact is missing, and 'Skip If no artifact is found' ${skip_on_missing_artifact}`
+    );
+    process.exit(0);
   }
 }
 
