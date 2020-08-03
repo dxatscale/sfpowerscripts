5d3d30e2ab4a94191ae8dff125d61b334b573ae7
Alan Ly <36793262+aly76@users.noreply.github.com>
2020-08-03 15:04:55 +1000

    Filter class coverage by package (#112)
    Fix Class Coverage by using classes in the package  to filter out dependency classes

--- packages/core/src/sfdxwrappers/TriggerApexTestImpl.ts
+++ packages/core/src/sfdxwrappers/TriggerApexTestImpl.ts
@@ -1,14 +1,15 @@
 import child_process = require("child_process");
 import { onExit } from "../OnExit";
-import { fail } from "assert";
 import { isNullOrUndefined } from "util";
 let fs = require("fs-extra");
 let path = require("path");
+import getMDAPIPackageFromSourceDirectory from "../getMdapiPackage";
 
 export default class TriggerApexTestImpl {
   public constructor(
     private target_org: string,
     private test_options: any,
+    private project_directory: string,
   ) {}
 
   public async exec(): Promise<{
@@ -17,8 +18,7 @@ export default class TriggerApexTestImpl {
     message: string;
   }> {
 
-
-    let test_result: { id: string; result: boolean; message: string } = {
+    const test_result: { id: string; result: boolean; message: string } = {
       id: "",
       result: false,
       message: ""
@@ -45,11 +45,14 @@ export default class TriggerApexTestImpl {
       });
 
       await onExit(child);
+
     } catch (err) {
 
+    } finally {
+      console.log(output);
     }
 
-
+    let test_report_json;
     try {
       let test_id = fs
         .readFileSync(
@@ -58,6 +61,7 @@ export default class TriggerApexTestImpl {
         .toString();
 
       console.log('test_id',test_id);
+      test_result.id = test_id;
 
       let test_report_json_file = fs
         .readFileSync(
@@ -68,11 +72,7 @@ export default class TriggerApexTestImpl {
         )
         .toString();
 
-      let test_report_json = JSON.parse(test_report_json_file);
-
-      test_result.id = test_id;
-
-
+      test_report_json = JSON.parse(test_report_json_file);
 
       //Print human readable output to console
       if ((test_report_json.summary.outcome == "Failed")) {
@@ -85,43 +85,28 @@ export default class TriggerApexTestImpl {
           }
         });
         test_result.result = false;
-        console.error(output);
-      } else {
-        const classesWithInvalidCoverage: string[] = [];
-
-        if (this.test_options["isValidateCoverage"]) {
-          let code_coverage = fs.readFileSync(
-            path.join(
-              this.test_options["outputdir"],
-              `test-result-codecoverage.json`
-            ),
-            "utf8"
-          );
-          let code_coverage_json = JSON.parse(code_coverage);
-
-          for (let testClass of code_coverage_json) {
-            if (testClass["coveredPercent"] < this.test_options["coverageThreshold"]) {
-              classesWithInvalidCoverage.push(testClass["name"]);
-            }
-          }
-        }
-
-        if (classesWithInvalidCoverage.length == 0) {
-          test_result.message = `${test_report_json.summary.passing} Tests passed with overall Test Run Coverage of ${test_report_json.summary.testRunCoverage}`;
-          test_result.result = true;
-        } else {
-          test_result.message=`The test classes ${classesWithInvalidCoverage.toString()} do not meet the required code coverage of ${this.test_options["coverageThreshold"]}`;
-          test_result.result = false;
-        }
-        console.log(output);
+        return test_result;
       }
-
-      return test_result;
     } catch (err) {
       test_result.result = false;
       test_result.message = error;
       return test_result;
     }
+
+    let classesWithInvalidCoverage: string[];
+
+    if (this.test_options["isValidateCoverage"]) {
+      classesWithInvalidCoverage = await this.validateClassCodeCoverage();
+    }
+
+    if (isNullOrUndefined(classesWithInvalidCoverage) || classesWithInvalidCoverage.length == 0) {
+      test_result.message = `${test_report_json.summary.passing} Tests passed with overall Test Run Coverage of ${test_report_json.summary.testRunCoverage} percent`;
+      test_result.result = true;
+    } else {
+      test_result.message=`The test classes ${classesWithInvalidCoverage.toString()} do not meet the required code coverage of ${this.test_options["coverageThreshold"]}`;
+      test_result.result = false;
+    }
+    return test_result;
   }
 
   private buildExecCommand(): string {
@@ -152,5 +137,92 @@ export default class TriggerApexTestImpl {
 
     console.log(`Generated Command: ${command}`);
     return command;
+  }
+
+  private async validateClassCodeCoverage(): Promise<string[]>  {
+    console.log(`Validating individual classes for code coverage greater than ${this.test_options["coverageThreshold"]} percent`);
+    let classesWithInvalidCoverage: string[] = [];
+
+    let packageClasses: string[] = await this.getClassesFromPackageManifest();
+
+    let code_coverage = fs.readFileSync(
+      path.join(
+        this.test_options["outputdir"],
+        `test-result-codecoverage.json`
+      ),
+      "utf8"
+    );
+    let code_coverage_json = JSON.parse(code_coverage);
+
+    code_coverage_json = this.filterCodeCoverageToPackageClasses(code_coverage_json, packageClasses);
+
+    for (let classCoverage of code_coverage_json) {
+      if (classCoverage["coveredPercent"] < this.test_options["coverageThreshold"]) {
+        classesWithInvalidCoverage.push(classCoverage["name"]);
+      }
+    }
+    return classesWithInvalidCoverage;
+  }
+
+  private filterCodeCoverageToPackageClasses(codeCoverage, packageClasses: string[]) {
+    let filteredCodeCoverage = codeCoverage;
+    if (!isNullOrUndefined(packageClasses)) {
+      // only include package classes in code coverage report
+      filteredCodeCoverage = codeCoverage.filter( (classCoverage) => {
+        for (let packageClass of packageClasses) {
+          if (packageClass == classCoverage["name"])
+            return true;
+        }
+        return false;
+      });
+    }
+    return filteredCodeCoverage;
+  }
+
+  private async getClassesFromPackageManifest(): Promise<string[]> {
+    let packageClasses: string[];
+
+    let packageDirectory: string = this.getPackageDirectory();
+
+    let mdapiPackage = await getMDAPIPackageFromSourceDirectory(
+      this.project_directory,
+      packageDirectory
+    );
+
+    for (let type of mdapiPackage["manifestAsJSON"]["Package"]["types"]) {
+      if (type["name"] == "ApexClass") {
+        packageClasses = type["members"];
+        break;
+      }
+    }
+    return packageClasses;
+  }
+
+  private getPackageDirectory(): string {
+    let packageDirectory: string;
+
+    let projectConfig: string;
+    if (!isNullOrUndefined(this.project_directory)) {
+      projectConfig = path.join(
+        this.project_directory,
+        "sfdx-project.json"
+      );
+    } else {
+      projectConfig = "sfdx-project.json";
+    }
+
+    let projectJson = JSON.parse(
+      fs.readFileSync(projectConfig, "utf8")
+    );
+
+    projectJson["packageDirectories"].forEach( (pkg) => {
+      if (this.test_options["packageToValidate"] == pkg["package"])
+        packageDirectory = pkg["path"];
+    });
+
+    if (isNullOrUndefined(packageDirectory))
+      throw new Error("Package or package directory to validate does not exist");
+    else
+      return packageDirectory;
   }
 }
