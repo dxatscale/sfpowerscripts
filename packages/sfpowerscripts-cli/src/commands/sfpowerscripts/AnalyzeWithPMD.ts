95fec953231c14adb337135506d5bb714bcc07ea
Alan Ly <36793262+aly76@users.noreply.github.com>
2020-08-06 13:50:04 +1000

    Update default PMD version to 6.26.0 (#119)

--- packages/sfpowerscripts-cli/src/commands/sfpowerscripts/AnalyzeWithPMD.ts
+++ packages/sfpowerscripts-cli/src/commands/sfpowerscripts/AnalyzeWithPMD.ts
@@ -35,7 +35,7 @@ export default class AnalyzeWithPMD extends SfdxCommand {
     rulesetpath: flags.string({description: messages.getMessage('rulesetPathFlagDescription')}),
     format: flags.string({description: messages.getMessage('formatFlagDescription'), options: ['text','textcolor','csv','emacs','summaryhtml','html','xml','xslt','yahtml','vbhtml','textpad'], default: 'text'}),
     outputpath: flags.string({char: 'o', description: messages.getMessage('outputPathFlagDescription')}),
-    version: flags.string({description: messages.getMessage('versionFlagDescription'), default: '6.22.0'}),
+    version: flags.string({description: messages.getMessage('versionFlagDescription'), default: '6.26.0'}),
     istobreakbuild: flags.boolean({char: 'b', description: messages.getMessage('isToBreakBuildFlagDescription')}),
     projectdir: flags.string({char: 'd', description: messages.getMessage('projectDirectoryFlagDescription')}),
     refname: flags.string({description: messages.getMessage('refNameFlagDescription')})
@@ -45,28 +45,28 @@ export default class AnalyzeWithPMD extends SfdxCommand {
   public async run(){
     try {
       console.log("Test.. PMD");
-  
+
       const project_directory = this.flags.projectdir;
       const source_directory: string = this.flags.sourcedir;
       const ruleset: string = this.flags.ruleset;
 
-  
+
       let rulesetpath=""
       if (ruleset == "Custom") {
         let rulesetpath = this.flags.rulesetpath;
         console.log(rulesetpath);
       }
-  
-      
-  
+
+
+
       const format: string = this.flags.format;
       const outputPath: string = this.flags.outputpath;
       const version: string = this.flags.version;
-  
+
       const isToBreakBuild = this.flags.istobreakbuild;
-  
+
       let result: [number, number, number] = [0, 0, 0];
-  
+
       let pmdImpl: AnalyzeWithPMDImpl = new AnalyzeWithPMDImpl(
         project_directory,
         source_directory,
@@ -77,7 +77,7 @@ export default class AnalyzeWithPMD extends SfdxCommand {
       );
       let command = await pmdImpl.buildExecCommand();
       await pmdImpl.exec(command);
-      
+
       if (!isNullOrUndefined(this.flags.refname)) {
         if (!isNullOrUndefined(outputPath)) {
             fs.writeFileSync('.env', `${this.flags.refname}_sfpowerscripts_pmd_output_path=${outputPath}\n`, {flag:'a'});
@@ -89,9 +89,9 @@ export default class AnalyzeWithPMD extends SfdxCommand {
           fs.writeFileSync('.env', `sfpowerscripts_pmd_output_path=${outputPath}\n`, {flag:'a'});
         } else {
           fs.writeFileSync('.env', `sfpowerscripts_pmd_output_path=${process.env.PWD}/pmd-output\n`, {flag:'a'});
-        }      
-      }   
-  
+        }
+      }
+
       let artifactFilePath = path.join(
       os.homedir(),
       "sfpowerkit",
@@ -99,25 +99,25 @@ export default class AnalyzeWithPMD extends SfdxCommand {
       `pmd-bin-${version}`,
       "sf-pmd-output.xml"
       );
-      
+
       if (fs.existsSync(artifactFilePath)) {
       result = parseXmlReport(artifactFilePath);
       }
-  
+
       if (isToBreakBuild && result[2] > 0)
           throw new SfdxError(`Build Failed due to ${result[2]} critical defects found`);
-                
+
     } catch (err) {
       console.log(err);
       // Fail the task when an error occurs
-      process.exit(1); 
+      process.exit(1);
     }
-    
+
     function parseXmlReport(xmlReport: string): [number, number, number] {
       let fileCount = 0;
       let violationCount = 0;
       let criticaldefects = 0;
-      
+
       let reportContent: string = fs.readFileSync(xmlReport, "utf-8");
       xml2js.parseString(reportContent, (err, data) => {
         // If the file is not XML, or is not from PMD, return immediately
@@ -125,19 +125,19 @@ export default class AnalyzeWithPMD extends SfdxCommand {
           console.debug(`Empty or unrecognized PMD xml report ${xmlReport}`);
           return null;
         }
-      
+
         if (!data.pmd.file || data.pmd.file.length === 0) {
           // No files with violations, return now that it has been marked for upload
           return null;
         }
-      
+
         data.pmd.file.forEach((file: any) => {
           if (file.violation) {
             fileCount++;
             violationCount += file.violation.length;
           }
         });
-      
+
         for (let i = 0; i < data.pmd.file.length; i++) {
           data.pmd.file[i].violation.forEach(element => {
             if (element["$"]["priority"] == 1) {
@@ -146,7 +146,7 @@ export default class AnalyzeWithPMD extends SfdxCommand {
           });
         }
       });
-      
+
       return [violationCount, fileCount, criticaldefects];
     }
   }
