0abfc742f95084fd91b5f5d54799698b336953d4
Alan Ly <alan.ly@accenture.com>
2020-08-29 15:02:55 +1000

    Add function for loading sfpowerscripts variables

---
+++ packages/sfpowerscripts-cli/src/VariableDictionary.ts
@@ -0,0 +1,11 @@
+export const sfpowerscripts_variable_dictionary = [
+    'sfpowerscripts_artifact_metadata_directory',
+    'sfpowerscripts_artifact_directory',
+    'sfpowerscripts_pmd_output_path',
+    'sfpowerscripts_delta_package_path',
+    'sfpowerscripts_package_version_number',
+    'sfpowerscripts_package_version_id',
+    'sfpowerkit_deploysource_id',
+    'sfpowerscripts_exportedsource_zip_path',
+    'sfpowerscripts_incremented_project_version'
+];
---
+++ packages/sfpowerscripts-cli/src/loadSfpowerscriptsVariables.ts
@@ -0,0 +1,20 @@
+import { sfpowerscripts_variable_dictionary } from "./VariableDictionary";
+
+/**
+ * @description substitutes CLI inputs, that match the variable dictionary, with
+ * the corresponding environment variable
+ * @param flags
+ */
+export default function loadSfpowerscriptsVariables(flags): void {
+    for (let flag in flags ) {
+        for ( let sfpowerscripts_variable of sfpowerscripts_variable_dictionary ) {
+            if (
+                typeof flags[flag] === "string" &&
+                flags[flag].includes(sfpowerscripts_variable)
+            ) {
+                console.log(`Substituting ${flags[flag]} with ${process.env[sfpowerscripts_variable]}`);
+                flags[flag] = process.env[sfpowerscripts_variable];
+            }
+        }
+    }
+}
