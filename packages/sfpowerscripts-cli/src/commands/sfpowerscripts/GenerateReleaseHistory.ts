942a3ba09827352caddf3c89762e417bdeac9272
Alan Ly <alan.ly@accenture.com>
2020-09-03 16:11:42 +1000

    Rename GenerateChangelog to GenerateReleaseHistory

--- packages/sfpowerscripts-cli/src/commands/sfpowerscripts/GenerateReleaseHistory.ts
+++ packages/sfpowerscripts-cli/src/commands/sfpowerscripts/GenerateReleaseHistory.ts
@@ -8,9 +8,9 @@ import {isNullOrUndefined} from "util"
 import simplegit, { SimpleGit } from "simple-git/promise";
 
 Messages.importMessagesDirectory(__dirname);
-const messages = Messages.loadMessages('@dxatscale/sfpowerscripts', 'generate_changelog');
+const messages = Messages.loadMessages('@dxatscale/sfpowerscripts', 'generate_release_history');
 
-export default class GenerateChangelog extends SfdxCommand {
+export default class GenerateReleaseHistory extends SfdxCommand {
 
   public static description = messages.getMessage('commandDescription');
 
@@ -22,11 +22,7 @@ export default class GenerateChangelog extends SfdxCommand {
   protected static requiresDevhubUsername = false;
 
   protected static flagsConfig = {
-    package: flags.string({char: 'n', description: messages.getMessage('packageFlagDescription')}),
     manifest: flags.directory({required: true, char: 'x', description: messages.getMessage('manifestFlagDescription')}),
-    revfrom: flags.string({required: false, char: 'r', description: messages.getMessage('revisionFromFlagDescription')}),
-    revto: flags.string({required: false, char: 't', description: messages.getMessage('revisionToFlagDescription')}),
-    symbol: flags.string({description: messages.getMessage('symbolFlagDescription')})
   };
 
 
@@ -103,7 +99,6 @@ export default class GenerateChangelog extends SfdxCommand {
 
 
 
-                // console.log(JSON.stringify(nextRelease, null, 4));
             }
 
             // Convert back to array for JSON stringify
@@ -115,78 +110,9 @@ export default class GenerateChangelog extends SfdxCommand {
 
         }
 
-
-        // for (let pkg of manifest["packages"]) {
-        //     let generateChangelogImpl: GenerateChangelogImpl = new GenerateChangelogImpl(
-        //         pkg["name"],
-        //         pkg["from"],
-        //         pkg["to"],
-        //         workItemFilter
-        //     );
-        //     let result = await generateChangelogImpl.exec();
-
-        //     // Merge with master changelog
-        //     for (let item in result["workItems"]) {
-        //         if (masterChangelog["workItems"][item] == null) {
-        //             masterChangelog["workItems"][item] = result["workItems"][item];
-        //         } else {
-        //             for (let commit of result["workItems"][item]) {
-        //                 masterChangelog["workItems"][item].add(commit);
-        //             }
-        //         }
-        //     }
-
-        //     masterChangelog["packages"].push(result["package"]);
-        // }
-
-        // Convert back to array for JSON stringify
-        // for (let key in masterChangelog["workItems"]) {
-        //     masterChangelog["workItems"][key] = Array.from(masterChangelog["workItems"][key]);
-        // }
-
-
         fs.writeFileSync(`releasechangelog.json`, JSON.stringify(masterChangelog, null, 4));
 
-        console.log(JSON.stringify(tagCommitIdMap,null,4));
-        // Generate Markdown
-        let payload: string = "";
-        for (let i = masterChangelog["releases"].length - 1 ; i >= 0 ; i-- ) {
-            let release = masterChangelog["releases"][i];
-            payload += `\n# ${release["name"]}\n`;
-
-            payload += "## Artifacts\n";
-            for (let artifact of release["artifacts"]) {
-                payload += `**${artifact["name"]}**     ${tagCommitIdMap[artifact["to"]]} (${artifact["to"]})\n\n`;
-            }
-
-            payload += "## Work Items\n";
-            for (let key in release["workItems"]) {
-                // TODO: Fix URL creation
-                let workItemURL: string;
-                if (manifest["workItemURL"] != null) {
-                    workItemURL = manifest["workItemURL"] + key
-                }
-                payload += `  - [${key}](${workItemURL})\n`
-            }
-
-            payload += "\n## Commits\n";
-            for (let artifact of release["artifacts"]) {
-                payload += `\n### ${artifact["name"]}\n`;
-                if (artifact["commits"].length > 0) {
-                    for (let commit of artifact["commits"]) {
-                        payload += `  - ${commit.date}      ${commit.commitId}      ${commit.message}\n`;
-                    }
-                } else if (artifact["from"] === artifact["to"]) {
-                    payload += `  - Artifact version has not changed\n`
-                } else {
-                    payload += ` - No changes to ${artifact["name"]} package directory detected. Artifact version may have been updated due to:\n`;
-                    payload += `    - Modified scratch org definition file\n`;
-                    payload += `    - Incremented package version in sfdx-project.json\n`;
-                    payload += `    - Build all packages\n`
-                }
-            }
-        }
-        fs.writeFileSync(`releasechangelog.md`, payload);
+        generateMarkdown(masterChangelog, manifest, tagCommitIdMap);
 
     } catch (err) {
       console.log(err);
@@ -194,4 +120,46 @@ export default class GenerateChangelog extends SfdxCommand {
       process.exit(1);
     }
   }
+}
+
+function generateMarkdown(masterChangelog, manifest, tagCommitIdMap): void {
+     // Generate Markdown
+     let payload: string = "";
+     for (let i = masterChangelog["releases"].length - 1 ; i >= 0 ; i-- ) {
+         let release = masterChangelog["releases"][i];
+         payload += `\n# ${release["name"]}\n`;
+
+         payload += "## Artifacts\n";
+         for (let artifact of release["artifacts"]) {
+             payload += `**${artifact["name"]}**     ${tagCommitIdMap[artifact["to"]]} (${artifact["to"]})\n\n`;
+         }
+
+         payload += "## Work Items\n";
+         for (let key in release["workItems"]) {
+             // TODO: Fix URL creation
+             let workItemURL: string;
+             if (manifest["workItemURL"] != null) {
+                 workItemURL = manifest["workItemURL"] + key
+             }
+             payload += `  - [${key}](${workItemURL})\n`
+         }
+
+         payload += "\n## Commits\n";
+         for (let artifact of release["artifacts"]) {
+             payload += `\n### ${artifact["name"]}\n`;
+             if (artifact["commits"].length > 0) {
+                 for (let commit of artifact["commits"]) {
+                     payload += `  - ${commit.date}      ${commit.commitId}      ${commit.message}\n`;
+                 }
+             } else if (artifact["from"] === artifact["to"]) {
+                 payload += `  - Artifact version has not changed\n`
+             } else {
+                 payload += ` - No changes to ${artifact["name"]} package directory detected. Artifact version may have been updated due to:\n`;
+                 payload += `    - Modified scratch org definition file\n`;
+                 payload += `    - Incremented package version in sfdx-project.json\n`;
+                 payload += `    - Build all packages\n`
+             }
+         }
+     }
+     fs.writeFileSync(`releasechangelog.md`, payload);
 }
