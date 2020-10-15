23c7c0d68f96e89f58f58fd0e63faba7c5f39b4e
sfpowerscripts <sfpowerscripts@dxscale>
2020-10-15 21:19:36 +1100

    Fix Retry for Pacakge Info

--- packages/core/src/sfdxwrappers/CreateUnlockedPackageImpl.ts
+++ packages/core/src/sfdxwrappers/CreateUnlockedPackageImpl.ts
@@ -8,11 +8,10 @@ import MDAPIPackageGenerator from "../generators/MDAPIPackageGenerator";
 import SFPLogger from "../utils/SFPLogger";
 const fs = require("fs-extra");
 import { EOL } from "os";
+import { delay } from "../utils/Delay";
 
 export default class CreateUnlockedPackageImpl {
-
-   private packageLogger;
- 
+  private packageLogger;
 
   public constructor(
     private sfdx_package: string,
@@ -27,8 +26,11 @@ export default class CreateUnlockedPackageImpl {
     private isSkipValidation: boolean,
     private packageArtifactMetadata: PackageMetadata
   ) {
-    fs.outputFileSync(`.sfpowerscripts/logs/${sfdx_package}`, `sfpowerscripts--log${EOL}`)
-    this.packageLogger=`.sfpowerscripts/logs/${sfdx_package}`;
+    fs.outputFileSync(
+      `.sfpowerscripts/logs/${sfdx_package}`,
+      `sfpowerscripts--log${EOL}`
+    );
+    this.packageLogger = `.sfpowerscripts/logs/${sfdx_package}`;
   }
 
   public async exec(): Promise<PackageMetadata> {
@@ -41,9 +43,9 @@ export default class CreateUnlockedPackageImpl {
     );
 
     let packageDirectory: string = packageDescriptor["path"];
-    SFPLogger.log("Package Directory", packageDirectory,this.packageLogger);
+    SFPLogger.log("Package Directory", packageDirectory, this.packageLogger);
 
-   //Resolve the package dependencies
+    //Resolve the package dependencies
     this.resolvePackageDependencies(packageDescriptor);
 
     //Redo the fetch of the descriptor as the above command would have redone the dependencies
@@ -60,17 +62,16 @@ export default class CreateUnlockedPackageImpl {
     );
     this.packageArtifactMetadata.payload = mdapiPackage.manifest;
 
-   
     let command = this.buildExecCommand();
-    SFPLogger.log("Package Creation Command", command,this.packageLogger);
-    let child = child_process.exec(
-      command,
-      { cwd: this.project_directory, encoding: "utf8" }
-    );
+    SFPLogger.log("Package Creation Command", command, this.packageLogger);
+    let child = child_process.exec(command, {
+      cwd: this.project_directory,
+      encoding: "utf8",
+    });
 
     let output = "";
     child.stdout.on("data", (data) => {
-      SFPLogger.log(data.toString(),null,this.packageLogger);
+      SFPLogger.log(data.toString(), null, this.packageLogger);
       output += data.toString();
     });
 
@@ -80,25 +81,7 @@ export default class CreateUnlockedPackageImpl {
     ).result.SubscriberPackageVersionId;
 
     //Get the full details on the package
-    SFPLogger.log("Fetching Version Number and Coverage details",null,this.packageLogger);
-    let pkgInfoResultAsJSON = child_process.execSync(
-      this.buildInfoCommand(this.packageArtifactMetadata.package_version_id),
-      {
-        cwd: this.project_directory,
-        encoding: "utf8",
-      }
-    );
-
-     SFPLogger.log("Package Info Fetched", pkgInfoResultAsJSON,this.packageLogger);
-
-    let pkgInfoResult = JSON.parse(pkgInfoResultAsJSON);
-    this.packageArtifactMetadata.isDependencyValidated = this.isSkipValidation;
-    this.packageArtifactMetadata.package_version_number =
-      pkgInfoResult.result[0].packageVersionNumber;
-    this.packageArtifactMetadata.test_coverage =
-      pkgInfoResult.result[0].coverage;
-    this.packageArtifactMetadata.has_passed_coverage_check =
-      pkgInfoResult.result[0].HasPassedCodeCoverageCheck;
+    await this.getPackageInfo();
 
     //Generate Source Artifact
     let mdapiPackageArtifactDir = SourcePackageGenerator.generateSourcePackageArtifact(
@@ -123,12 +106,59 @@ export default class CreateUnlockedPackageImpl {
       timestamp: Date.now(),
     };
 
-
     return this.packageArtifactMetadata;
   }
 
+  private async getPackageInfo() {
+
+    
+    while (true) {
+      try {
+        SFPLogger.log(
+          "Fetching Version Number and Coverage details",
+          null,
+          this.packageLogger
+        );
+        let pkgInfoResultAsJSON = child_process.execSync(
+          this.buildInfoCommand(
+            this.packageArtifactMetadata.package_version_id
+          ),
+          {
+            cwd: this.project_directory,
+            encoding: "utf8",
+          }
+        );
+
+        SFPLogger.log(
+          "Package Info Fetched",
+          pkgInfoResultAsJSON,
+          this.packageLogger
+        );
+
+        let pkgInfoResult = JSON.parse(pkgInfoResultAsJSON);
+        this.packageArtifactMetadata.isDependencyValidated = this.isSkipValidation;
+        this.packageArtifactMetadata.package_version_number =
+          pkgInfoResult.result[0].packageVersionNumber;
+        this.packageArtifactMetadata.test_coverage =
+          pkgInfoResult.result[0].coverage;
+        this.packageArtifactMetadata.has_passed_coverage_check =
+          pkgInfoResult.result[0].HasPassedCodeCoverageCheck;
+          break;
+      } catch (error) {
+        SFPLogger.log(
+          "Unable to fetch package version info",
+          null,
+          this.packageLogger
+        );
+        console.log('Retrying...')
+        await delay(2000);
+        continue;
+      }
+    }
+  }
+
   private resolvePackageDependencies(packageDescriptor: any) {
-   SFPLogger.log("Resolving project dependencies",null,this.packageLogger);
+    SFPLogger.log("Resolving project dependencies", null, this.packageLogger);
     let resolveResult;
     if (this.isSkipValidation) {
       let resolveResult;
@@ -136,14 +166,13 @@ export default class CreateUnlockedPackageImpl {
         `sfdx sfpowerkit:package:dependencies:list -p ${packageDescriptor["path"]} -v ${this.devhub_alias} -w`,
         { cwd: this.project_directory, encoding: "utf8" }
       );
-    }
-    else {
+    } else {
       resolveResult = child_process.execSync(
         `sfdx sfpowerkit:package:dependencies:list -p ${packageDescriptor["path"]} -v ${this.devhub_alias} -w --usedependencyvalidatedpackages`,
         { cwd: this.project_directory, encoding: "utf8" }
       );
     }
-    SFPLogger.log(resolveResult,null,this.packageLogger);
+    SFPLogger.log(resolveResult, null, this.packageLogger);
   }
 
   private buildExecCommand(): string {
