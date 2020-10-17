ec9a1969743401f06551093bed203ebfda2ace9c
azlam.abdulsalam <azlam.abdulsalam@accenture.com>
2020-10-17 16:32:32 +1100

    Fix incorrect refactoring

--- packages/core/src/parallelBuilder/BuildImpl.ts
+++ packages/core/src/parallelBuilder/BuildImpl.ts
@@ -531,4 +531,11 @@ export default class BuildImpl {
 
     return result;
   }
+  
+  private getFormattedTime(milliseconds: number): string {
+    let date = new Date(0);
+    date.setSeconds(milliseconds / 1000); // specify value for SECONDS here
+    let timeString = date.toISOString().substr(11, 8);
+    return timeString;
+  }
 }
