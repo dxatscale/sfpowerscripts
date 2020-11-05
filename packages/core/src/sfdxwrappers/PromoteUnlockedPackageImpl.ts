3cc70350b24a7475036571de61b9c49ed4c83a56
Alan Ly <36793262+aly76@users.noreply.github.com>
2020-11-06 10:48:12 +1100

    Add Postpack command (#198)
    * Add Postpack command
    
    * Log stderr stream in PromoteUnlockedPackageImpl

--- packages/core/src/sfdxwrappers/PromoteUnlockedPackageImpl.ts
+++ packages/core/src/sfdxwrappers/PromoteUnlockedPackageImpl.ts
@@ -6,14 +6,16 @@ export default class PromoteUnlockedPackageImpl {
   public constructor(private  project_directory:string,private package_version_id: string, private devhub_alias: string) {}
 
   public async exec(): Promise<void> {
-    
+
     let command = this.buildExecCommand();
     SFPLogger.log("Executing command",command);
-    let child = child_process.exec(command, { cwd: this.project_directory, encoding: "utf8" },(error, stdout, stderr) => {
-      if (error) throw error;
-    });
+    let child = child_process.exec(command, { cwd: this.project_directory, encoding: "utf8" });
 
     child.stdout.on("data", data => {
+      SFPLogger.log(data.toString());
+    });
+
+    child.stderr.on("data", data => {
       SFPLogger.log(data.toString());
     });
 
@@ -25,7 +27,7 @@ export default class PromoteUnlockedPackageImpl {
     //package
     command += ` -p ${this.package_version_id}`;
     //noprompt
-    command += ` -n`;   
+    command += ` -n`;
     return command;
   }
 }
