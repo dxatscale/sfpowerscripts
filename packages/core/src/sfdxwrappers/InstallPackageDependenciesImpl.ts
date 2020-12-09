d0f9d3bceb10fd1144e5d3de5807f9e8ce06fbde
Alan Ly <alan.ly@accenture.com>
2020-12-09 18:29:23 +1100

    Temporarily suppress logs in InstallPackageDependenciesImpl

--- packages/core/src/sfdxwrappers/InstallPackageDependenciesImpl.ts
+++ packages/core/src/sfdxwrappers/InstallPackageDependenciesImpl.ts
@@ -19,7 +19,7 @@ export default class InstallPackageDepenciesImpl {
   ) {}
 
   public async exec(): Promise<PackageInstallationResult> {
-
+    SFPLogger.isSupressLogs = true;
     let command = `sfdx sfpowerkit:package:dependencies:install -u ${this.target_org} -v ${this.devhub_alias} -r -w ${this.wait_time}`;
 
     if (this.apexcompileonlypackage) command += ` -a`;
