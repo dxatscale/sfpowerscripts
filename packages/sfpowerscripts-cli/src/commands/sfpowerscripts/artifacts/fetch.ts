943b01ea036277a95e21b348d53c0f9081d535d9
Alan Ly <36793262+aly76@users.noreply.github.com>
2021-03-19 11:21:02 +1100

    Utility command for fetching artifacts from registry  (#451)
    * Fetch artifacts from registry
    
    * Remove --npmtag flag
    
    * Add ReleaseDefinition interface
    
    * Remove empty finally block
    
    * Update examples
    
    * Remove debugging statements
    
    * Update README.md
    
    * Fix typo in regexp
    
    * Fix typo in import
    
    * Update messages

---
+++ packages/sfpowerscripts-cli/src/commands/sfpowerscripts/artifacts/fetch.ts
@@ -0,0 +1,78 @@
+import { flags } from '@salesforce/command';
+import SfpowerscriptsCommand from '../../../SfpowerscriptsCommand';
+import { Messages } from '@salesforce/core';
+import FetchImpl from '../../../impl/artifacts/FetchImpl';
+
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('@dxatscale/sfpowerscripts', 'fetch');
+
+export default class Fetch extends SfpowerscriptsCommand {
+
+  public static description = messages.getMessage('commandDescription');
+
+  public static examples = [
+    `$ sfdx sfpowerscripts:artifacts:fetch -p myreleasedefinition.yaml -f myscript.sh`,
+    `$ sfdx sfpowerscripts:artifacts:fetch -p myreleasedefinition.yaml --npm --scope myscope --npmrcpath path/to/.npmrc`
+  ];
+
+  protected static requiresUsername = false;
+  protected static requiresDevhubUsername = false;
+
+  protected static flagsConfig = {
+    releasedefinition: flags.filepath({
+      char: "p",
+      description: messages.getMessage('releaseDefinitionFlagDescription')
+    }),
+    artifactdir: flags.directory({
+      required: true,
+      char: 'd',
+      description: messages.getMessage('artifactDirectoryFlagDescription'),
+      default: 'artifacts'
+    }),
+    scriptpath: flags.filepath({
+      char: 'f',
+      description: messages.getMessage('scriptPathFlagDescription')
+    }),
+    npm: flags.boolean({
+      description: messages.getMessage('npmFlagDescription'),
+      exclusive: ['scriptpath']
+    }),
+    scope: flags.string({
+      description: messages.getMessage('scopeFlagDescription'),
+      dependsOn: ['npm'],
+      parse: (scope) => scope.replace(/@/g,"").toLowerCase()
+    }),
+    npmrcpath: flags.filepath({
+      description: messages.getMessage('npmrcPathFlagDescription'),
+      dependsOn: ['npm'],
+      required: false
+    })
+  };
+
+
+  public async execute(){
+    this.validateFlags();
+
+    try {
+      let fetchImpl: FetchImpl = new FetchImpl(
+        this.flags.releasedefinition,
+        this.flags.artifactdir,
+        this.flags.scriptpath,
+        this.flags.npm,
+        this.flags.scope,
+        this.flags.npmrcpath
+      );
+      fetchImpl.exec();
+    } catch (err) {
+      console.log(err.message);
+
+      // Fail the task when an error occurs
+      process.exitCode = 1;
+    }
+  }
+
+  private validateFlags() {
+    if (this.flags.npm && !this.flags.scope)
+      throw new Error("--scope parameter is required for NPM");
+  }
+}
