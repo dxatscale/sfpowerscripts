664d0ef8c2d9d43242e97dc9a2be1355da7518a7
azlam <azlam.abdulsalam@accenture.com>
2021-03-25 15:53:12 +1100

    Add error message on validate

--- packages/sfpowerscripts-cli/src/impl/validate/ValidateImpl.ts
+++ packages/sfpowerscripts-cli/src/impl/validate/ValidateImpl.ts
@@ -236,9 +236,15 @@ export default class ValidateImpl {
 
     let { generatedPackages, failedPackages } = await buildImpl.exec();
 
+
     if (failedPackages.length > 0)
       throw new Error(`Failed to create source packages ${failedPackages}`);
 
+
+   if(generatedPackages.length == 0)
+   {
+      throw new Error(`No changes detected in the packages to be built \n, validate will only execute if there is a change in atleast one of the packages`)
+   }
 
     for (let generatedPackage of generatedPackages) {
       try {
