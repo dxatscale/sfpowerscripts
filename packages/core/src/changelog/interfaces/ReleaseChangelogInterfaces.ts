126bc101f2104f1b6aa2b6a30c9536106441205b
Alan Ly <alan.ly@accenture.com>
2021-04-15 17:53:06 +1000

    Add comments to changelog interfaces

--- packages/core/src/changelog/interfaces/ReleaseChangelogInterfaces.ts
+++ packages/core/src/changelog/interfaces/ReleaseChangelogInterfaces.ts
@@ -11,10 +11,33 @@ export interface Release {
 }
 
 export interface Artifact extends Changelog {
-    name: string,
-    from: string,
-    to: string,
-    version: string,
-    latestCommitId: string,
-    commits: Commit[]
+    /**
+     * Name of the artifact
+     */
+    name: string;
+
+    /**
+     * Commit Id from which previous artifact was created
+     */
+    from: string;
+
+    /**
+     * Commit Id from which current artifact was created
+     */
+    to: string;
+
+    /**
+     * Package version number
+     */
+    version: string;
+
+    /**
+     * Latest commit Id in the package changelog
+     */
+    latestCommitId: string;
+
+    /**
+     * Commits between previous artifact's package changelog and current artifact's package changelog
+     */
+    commits: Commit[];
 }
