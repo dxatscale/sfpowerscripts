028d11bb7d57430dc8e72d4ea2569f462ac1034a
Alan Ly <36793262+aly76@users.noreply.github.com>
2021-05-04 16:27:19 +1000

    Generate changelog in orchestrator:release (#511)
    * Refactor ChangelogImpl
    
    * Change logic for finding latestCommitId for artifacts
    
    Find first occurence of latestCommitId for an artifact, instead of checking the
    preceding release only
    
    * Add comments to changelog interfaces
    
    * Rename fromIdx variable to indexOfLatestCommitId
    
    Refactor to method getArtifactsToLatestCommitId()
    
    * Rename latestReleaseDefinition
    
    * Refactor into functions
    
    * Render latest release in terminal
    
    * Temporarily remove branch from sfdx command for unlocked packaging
    
    Waiting for fix in Sfpowerkit for listing dependencies by branch
    
    * Make 'branch' required in build command
    
    * Remove branch parameter from Changelog
    
    Automatically create branch based on source branch of artifacts
    
    * Add Org info to changelog
    
    Add --generatechangelog flag to orchestrator:release
    
    Use hashId to determine whether release exists in changelog
    
    Remove releaseOptions nesting
    
    Render badges in changelog markdown
    
    Indent artifacts in changelog markdown
    
    Add emoji's to changelog markdown
    
    * Support multiple release names for the same hashId
    
    * Change org.releases into an array of ReleaseId's
    
    To trace orgs that a release has been deployed to
    
    * Add changelog log group
    
    * Print commits as table
    
    * Change indexOfLatestRelease to latestRelease in ReleaseChangelog interface
    
    Keep a copy of the latestRelease instead of storing the index of the latest
    release
    
    --showallartifacts true by default
    
    * Refactor into classes
    
    * Add unit tests
    
    Fix bug in latestCommitId not being updated for empty commits
    
    * Add unit tests for OrgUpdater
    
    * Remove generateValidAnchor
    
    No longer required, as anchor is now based on hashId which is in hexadecimal
    
    * Fix ReleaseDefinition tests
    
    * Remove unused imports
    
    * Toggle OrgUpdater for changelog:generate compatibility
    
    Fix new release name for old release
    
    * Fix retryCount logic
    
    Move OrgsUpdater to bottom of ReleaseChangelogUpdater
    
    * Retry call to ChangelogImpl
    
    * Move retry to ChangelogImpl
    
    * Update packages/core/src/sfpcommands/package/CreateUnlockedPackageImpl.ts
    
    Marking it as TODO
    
    * Create ChangelogMarkdownGenerator class
    
    Move ReleaseChangelog interfaces to CLI module
    
    Co-authored-by: Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>

--- packages/sfpowerscripts-cli/tests/impl/release/ReleaseDefinition.test.ts
+++ packages/sfpowerscripts-cli/tests/impl/release/ReleaseDefinition.test.ts
@@ -118,14 +118,13 @@ describe("Given a release definition, validateReleaseDefinition", () => {
     expect(() => { new ReleaseDefinition(null); }).toThrow();
   });
 
-  it("should not throw an error for valid release options", () => {
+  it("should not throw an error for valid release parameters", () => {
     releaseDefinitionYaml = `
       release: "test-release"
+      skipIfAlreadyInstalled: true
+      baselineOrg: "prod"
       artifacts:
         packageA: "3.0.5-13"
-      releaseOptions:
-        skipIfAlreadyInstalled: true
-        baselineOrg: "prod"
     `;
 
     expect(() => { new ReleaseDefinition(null); }).not.toThrow();
@@ -134,13 +133,55 @@ describe("Given a release definition, validateReleaseDefinition", () => {
   it("should throw an error if baselineOrg specified but skipIfAlreadyInstalled is false", () => {
     releaseDefinitionYaml = `
       release: "test-release"
+      skipIfAlreadyInstalled: false
+      baselineOrg: "prod"
       artifacts:
         packageA: "3.0.5-13"
-      releaseOptions:
-        skipIfAlreadyInstalled: false
-        baselineOrg: "prod"
     `;
 
+    expect(() => { new ReleaseDefinition(null); }).toThrow();
+  });
+
+  it("should not throw an error for valid changelog parameters", () => {
+    releaseDefinitionYaml = `
+      release: "test-release"
+      artifacts:
+        packageA: "3.0.5-13"
+      changelog:
+        repoUrl: "https://github.com/dxatscale/easy-spaces-lwc.git"
+        workItemFilter: "GOR-[0-9]{4}"
+        workItemUrl: "https://www.atlassian.com/software/jira"
+        limit: 10
+        showAllArtifacts: false
+    `;
+
+    expect(() => { new ReleaseDefinition(null); }).not.toThrow();
+  });
+
+  it("should throw an error if required changelog parameters are missing", () => {
+    releaseDefinitionYaml = `
+      release: "test-release"
+      artifacts:
+        packageA: "3.0.5-13"
+      changelog:
+        repoUrl: "https://github.com/dxatscale/easy-spaces-lwc.git"
+        workItemUrl: "https://www.atlassian.com/software/jira"
+        limit: 10
+        showAllArtifacts: false
+    `;
+
+    expect(() => { new ReleaseDefinition(null); }).toThrow();
+
+    releaseDefinitionYaml = `
+      release: "test-release"
+      artifacts:
+        packageA: "3.0.5-13"
+      changelog:
+        workItemFilter: "GOR-[0-9]{4}"
+        workItemUrl: "https://www.atlassian.com/software/jira"
+        limit: 10
+        showAllArtifacts: false
+    `;
     expect(() => { new ReleaseDefinition(null); }).toThrow();
   });
 });
