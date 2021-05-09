874dd8cb34adfee5c12afd5c8e9d57981eafed3e
Alan Ly <alan.ly@accenture.com>
2021-05-09 14:09:49 +1000

    Fix OrgsUpdater picking up new releases as old
    Pass the releaseWithMatchingHashId to OrgsUpdater instead of determining itself
    whether is new release
    
    Fix branch flag on changelog:generate so that it's not required

--- packages/sfpowerscripts-cli/src/commands/sfpowerscripts/changelog/generate.ts
+++ packages/sfpowerscripts-cli/src/commands/sfpowerscripts/changelog/generate.ts
@@ -48,7 +48,7 @@ export default class GenerateChangelog extends SfdxCommand {
       description: messages.getMessage('repoUrlFlagDescription')
     }),
     branchname: flags.string({
-      required: true,
+      required: false,
       char: "b",
       description: messages.getMessage('branchNameFlagDescription'),
       deprecated: { messageOverride: "--branchname has been deprecated" },
