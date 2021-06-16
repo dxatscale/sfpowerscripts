df0f046aa021d3a4c03688e3dbc9f7bc2e7a4abc
sfpowerscripts <sfpowerscripts@dxscale>
2021-06-16 16:01:11 +1000

    Add proper error handling to artifact selector

--- packages/sfpowerscripts-cli/src/impl/artifacts/FetchArtifactSelector.ts
+++ packages/sfpowerscripts-cli/src/impl/artifacts/FetchArtifactSelector.ts
@@ -4,19 +4,23 @@ import { FetchAnArtifactUsingScript } from "./FetchAnArtifactUsingScript";
 
 export default class FetchArtifactSelector {
   constructor(
-    private fetchArtifactScript: string,
-    private scope: string,
-    private npmrcPath: string
+    private fetchArtifactScript?: string,
+    private scope?: string,
+    private npmrcPath?: string
   ) {}
 
   public getArtifactFetcher(): FetchAnArtifact {
     if (this.fetchArtifactScript) {
       return new FetchAnArtifactUsingScript(this.fetchArtifactScript);
-    } else {
+    } else if(this.scope) {
       return new FetchAnArtifactFromNPM(
         this.scope,
         this.npmrcPath
       );
+    }
+    else
+    {
+      throw new Error("Unable to determine artifact fetcher");
     }
   }
 }
