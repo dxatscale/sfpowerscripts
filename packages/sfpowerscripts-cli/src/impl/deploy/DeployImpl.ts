dxatscale/sfpowerscripts

commit 5e87e3685b549486f75a8564f59f8b968536fc7f

Fix Incorrect trigger test path

packages/sfpowerscripts-cli/src/impl/deploy/DeployImpl.ts
@@ -205,6 +205,8 @@ export default class DeployImpl {
           throw new Error(packageInstallationResult.message);
         }
 
+
+        //Trigger Tests for Validate Deployment
         if (this.props.isTestsToBeTriggered) {
           if (packageMetadata.isApexFound) {
             if (!queue[i].skipTesting) {
@@ -214,6 +216,7 @@ export default class DeployImpl {
               try {
                 testResult = await this.triggerApexTests(
                   queue[i].package,
+                  packageInfo.sourceDirectory,
                   this.props.targetUsername,
                   queue[i].skipCoverageValidation,
                   this.props.coverageThreshold
@@ -252,6 +255,8 @@ export default class DeployImpl {
             }
           }
         }
+
+        
       }
 
       return {
@@ -661,6 +666,7 @@ export default class DeployImpl {
 
   private async triggerApexTests(
     sfdx_package: string,
+    projectDirectory:string,
     targetUsername: string,
     skipCoverageValidation: boolean,
     coverageThreshold: number
@@ -670,10 +676,10 @@ export default class DeployImpl {
     message: string;
   }> {
     let sfPackage: SFPPackage = await SFPPackage.buildPackageFromProjectConfig(
-      null,
+      this.props.packageLogger,
+      projectDirectory,
       sfdx_package,
-      null,
-      this.props.packageLogger
+      null
     );
     let testOptions: TestOptions = new RunAllTestsInPackageOptions(
       sfPackage,

packages/core/src/org/OrgDetails.ts
@@ -16,7 +16,6 @@ export default class OrgDetails {
             );
             let result = JSON.parse(cmdOutput);
             if (result["status"] == 0) {
-              SFPLogger.log(result["result"]["records"][0]);
               return result["result"]["records"][0];
             }
             else

packages/core/src/sfpcommands/package/InstallSourcePackageImpl.ts
@@ -474,7 +474,7 @@ export default class InstallSourcePackageImpl {
           ` --------------------------------------WARNING! SKIPPING TESTS-------------------------------------------------${EOL}` +
             `Skipping tests for deployment to sandbox. Be cautious that deployments to prod will require tests and >75% code coverage ${EOL}` +
             `-------------------------------------------------------------------------------------------------------------`,
-            LoggerLevel.WARN,
+            LoggerLevel.DEBUG,
             this.packageLogger
         );
         mdapi_options["testlevel"] = "NoTestRun";

commit 0238a087c7f31fd9a73a8fe7c7ee12f5fc038644

Fix bug introduced in 5e87e36
project directory should be null for buildPackageFromProjectConfig in
triggerApexTests

packages/sfpowerscripts-cli/src/impl/deploy/DeployImpl.ts
@@ -216,7 +216,6 @@ export default class DeployImpl {
               try {
                 testResult = await this.triggerApexTests(
                   queue[i].package,
-                  packageInfo.sourceDirectory,
                   this.props.targetUsername,
                   queue[i].skipCoverageValidation,
                   this.props.coverageThreshold
@@ -256,7 +255,7 @@ export default class DeployImpl {
           }
         }
 
-        
+
       }
 
       return {
@@ -666,7 +665,6 @@ export default class DeployImpl {
 
   private async triggerApexTests(
     sfdx_package: string,
-    projectDirectory:string,
     targetUsername: string,
     skipCoverageValidation: boolean,
     coverageThreshold: number
@@ -677,7 +675,7 @@ export default class DeployImpl {
   }> {
     let sfPackage: SFPPackage = await SFPPackage.buildPackageFromProjectConfig(
       this.props.packageLogger,
-      projectDirectory,
+      null,
       sfdx_package,
       null
     );