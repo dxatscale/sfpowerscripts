b1f3161edce46d4066837cf43a64d54b061f2a3a
Alan Ly <alan.ly@accenture.com>
2021-07-01 21:16:14 +1000

    Refactor source tracking logic into own class

--- packages/sfpowerscripts-cli/src/impl/prepare/PrepareOrgJob.ts
+++ packages/sfpowerscripts-cli/src/impl/prepare/PrepareOrgJob.ts
@@ -23,12 +23,8 @@ import ProjectConfig from "@dxatscale/sfpowerscripts.core/lib/project/ProjectCon
 import { PoolConfig } from "../pool/PoolConfig";
 import { Result, ok, err } from "neverthrow";
 import { ArtifactFilePaths } from "@dxatscale/sfpowerscripts.core/lib/artifacts/ArtifactFilePathFetcher";
-const path = require("path");
-import * as fs from "fs-extra";
-import lodash = require("lodash");
-import AdmZip = require("adm-zip");
-import child_process = require("child_process");
 import RelaxIPRange from "@dxatscale/sfpowerscripts.core/lib/iprange/RelaxIPRange"
+import SourceTrackingResource from "./SourceTrackingResource";
 
 const SFPOWERSCRIPTS_ARTIFACT_PACKAGE = "04t1P000000ka9mQAA";
 export default class PrepareOrgJob extends PoolJobExecutor {
@@ -107,17 +103,20 @@ export default class PrepareOrgJob extends PoolJobExecutor {
 
       if (this.artifacts) {
         let deploymentResult: DeploymentResult;
-        if (true) {
-          deploymentResult = await this.pushArtifacts(
-            scratchOrg,
-            packageLogger
-          );
+
+        let deploymentMode: DeploymentMode;
+        if (this.pool.deploymentType === "mdapi") {
+          deploymentMode = DeploymentMode.SOURCEPACKAGES;
         } else {
-          deploymentResult = await this.deployAllPackagesInTheRepo(
-            scratchOrg,
-            packageLogger
-          );
+          deploymentMode = DeploymentMode.SOURCEPACKAGES_PUSH
         }
+
+        deploymentResult = await this.deployAllPackagesInTheRepo(
+          scratchOrg,
+          packageLogger,
+          deploymentMode
+        );
+
         this.pool.succeedOnDeploymentErrors
           ? this.handleDeploymentErrorsForPartialDeployment(
               scratchOrg,
@@ -130,120 +129,9 @@ export default class PrepareOrgJob extends PoolJobExecutor {
               packageLogger
             );
 
-        // consolidate source tracking files
-        // TODO: only run for push
-        let aggregatedSourceTrackingDir = ".sfpowerscripts/sourceTrackingFiles";
-        let aggregatedUsernameDir = path.join(aggregatedSourceTrackingDir, scratchOrg.username);
-        let aggregatedMaxRevisionFilePath = path.join(aggregatedUsernameDir, "maxRevision.json");
-        let aggregatedSourcePathinfosFilePath = path.join(aggregatedUsernameDir, "sourcePathInfos.json");
-
-        fs.mkdirpSync(aggregatedUsernameDir);
-
-        for (let packageInfoOfDeployedArtifact of deploymentResult.deployed) {
-          console.log(packageInfoOfDeployedArtifact.sourceDirectory);
-          let orgsDir = path.join(packageInfoOfDeployedArtifact.sourceDirectory, ".sfdx", "orgs");
-          let usernameDir = path.join(orgsDir, scratchOrg.username);
-
-          if (!fs.existsSync(usernameDir)) throw new Error(`Failed to consolidate source tracking files. Unable to find ${usernameDir}`);
-
-          let maxRevisionFilePath = path.join(usernameDir, "maxRevision.json");
-          let sourcePathInfosFilePath = path.join(usernameDir, "sourcePathInfos.json");
-
-          if (!fs.existsSync(maxRevisionFilePath) || !fs.existsSync(sourcePathInfosFilePath))
-            throw new Error(`Failed to consolidate source tracking files. Missing source tracking files`);
-
-          if (fs.existsSync(aggregatedMaxRevisionFilePath) && fs.existsSync(aggregatedSourcePathinfosFilePath)) {
-
-            let aggregatedMaxRevision = fs.readJSONSync(aggregatedMaxRevisionFilePath, {encoding: "UTF-8"});
-            let maxRevision = fs.readJSONSync(maxRevisionFilePath, {encoding: "UTF-8"})
-            if (maxRevision.serverMaxRevisionCounter >= aggregatedMaxRevision.serverMaxRevisionCounter) {
-              // Replace maxRevision.json
-              fs.copySync(maxRevisionFilePath, aggregatedMaxRevisionFilePath, { overwrite: true });
-            }
-
-            // Concatenate sourcePathInfos.json
-            let aggregatedSourcePathInfos = fs.readJSONSync(aggregatedSourcePathinfosFilePath, {encoding: "UTF-8"});
-            let sourcePathInfos = fs.readJSONSync(sourcePathInfosFilePath, {encoding: "UTF-8"});
-
-            // truncate source path
-            for (let entry of Object.entries<any>(sourcePathInfos)) {
-              let newPropName = entry[0].replace(path.resolve(packageInfoOfDeployedArtifact.sourceDirectory), "");
-              let newPropValue = lodash.cloneDeep(entry[1]);
-              newPropValue.sourcePath = newPropValue.sourcePath.replace(path.resolve(packageInfoOfDeployedArtifact.sourceDirectory), "");
-
-              sourcePathInfos[newPropName] = newPropValue;
-
-              delete sourcePathInfos[entry[0]];
-            }
-
-            Object.assign(aggregatedSourcePathInfos, sourcePathInfos)
-
-            fs.writeJSONSync(aggregatedSourcePathinfosFilePath, aggregatedSourcePathInfos, {spaces: 2});
-          } else {
-            fs.copySync(maxRevisionFilePath, aggregatedMaxRevisionFilePath);
-            fs.copySync(sourcePathInfosFilePath, aggregatedSourcePathinfosFilePath);
-
-            let aggregatedSourcePathInfos = fs.readJSONSync(aggregatedSourcePathinfosFilePath, {encoding: "UTF-8"});
-            // truncate source path
-            for (let entry of Object.entries<any>(aggregatedSourcePathInfos)) {
-              let newPropName = entry[0].replace(path.resolve(packageInfoOfDeployedArtifact.sourceDirectory), "");
-              let newPropValue = lodash.cloneDeep(entry[1]);
-              newPropValue.sourcePath = newPropValue.sourcePath.replace(path.resolve(packageInfoOfDeployedArtifact.sourceDirectory), "");
-              aggregatedSourcePathInfos[newPropName] = newPropValue;
-
-              delete aggregatedSourcePathInfos[entry[0]];
-            }
-
-            fs.writeJSONSync(aggregatedSourcePathinfosFilePath, aggregatedSourcePathInfos, { spaces: 2 })
-          }
-        }
-
-        // Deploy static resource to SO
-        let projectConfig = {
-          packageDirectories: [
-            {
-              path: "force-app",
-              default: true
-            }
-          ],
-          namespace: "",
-          sourceApiVersion: "49.0"
-        };
-
-        fs.writeJSONSync(path.join(aggregatedUsernameDir, "sfdx-project.json"), projectConfig, { spaces: 2 });
-        let staticResourcesDir = path.join(aggregatedUsernameDir, "force-app", "main", "default", "staticresources");
-        fs.mkdirpSync(staticResourcesDir);
-
-        let zip = new AdmZip();
-        zip.addLocalFile(aggregatedMaxRevisionFilePath);
-        zip.addLocalFile(aggregatedSourcePathinfosFilePath);
-        zip.writeZip(path.join(staticResourcesDir, "sourceTrackingFiles.zip"));
-
-        let metadataXml: string =
-          `<?xml version="1.0" encoding="UTF-8"?>
-          <StaticResource xmlns="http://soap.sforce.com/2006/04/metadata">
-            <cacheControl>Public</cacheControl>
-            <contentType>application/zip</contentType>
-          </StaticResource>`;
-
-        fs.writeFileSync(path.join(staticResourcesDir, "sourceTrackingFiles.resource-meta.xml"), metadataXml);
-
-        try {
-          child_process.execSync(
-            `sfdx force:source:deploy -p force-app -u ${scratchOrg.username}`,
-            {
-              cwd: aggregatedUsernameDir,
-              encoding: 'utf8',
-              stdio: 'pipe'
-            }
-          );
-        } catch (error) {
-          SFPLogger.log(
-            `Failed to deploy static resources to scratch org`,
-            null,
-            packageLogger
-          );
-          throw error;
+        if (deploymentMode === DeploymentMode.SOURCEPACKAGES_PUSH) {
+          let sourceTrackingResource = new SourceTrackingResource(scratchOrg, deploymentResult, packageLogger);
+          sourceTrackingResource.deploy();
         }
 
 
@@ -260,34 +148,10 @@ export default class PrepareOrgJob extends PoolJobExecutor {
     }
   }
 
-  private async pushArtifacts(
-    scratchOrg: ScratchOrg,
-    packageLogger: any
-  ) {
-
-    let deployProps: DeployProps = {
-      deploymentMode: DeploymentMode.SOURCEPACKAGES_PUSH,
-      artifacts: this.artifacts,
-      targetUsername: scratchOrg.username,
-      artifactDir: null,
-      isTestsToBeTriggered: false,
-      skipIfPackageInstalled: false,
-      waitTime: 120,
-      packageLogger: packageLogger,
-      currentStage: Stage.PREPARE,
-      isRetryOnFailure: this.pool.retryOnFailure
-    }
-
-    //Deploy the fetched artifacts to the org
-    let deployImpl: DeployImpl = new DeployImpl(deployProps);
-
-    let deploymentResult = await deployImpl.exec();
-
-    return deploymentResult;
-  }
   private async deployAllPackagesInTheRepo(
     scratchOrg: ScratchOrg,
-    packageLogger: any
+    packageLogger: any,
+    deploymentMode: DeploymentMode
   ) {
     SFPLogger.log(`Deploying all packages in the repo to  ${scratchOrg.alias}`);
     SFPLogger.log(
@@ -304,7 +168,7 @@ export default class PrepareOrgJob extends PoolJobExecutor {
       packageLogger: packageLogger,
       isTestsToBeTriggered: false,
       skipIfPackageInstalled: false,
-      deploymentMode: DeploymentMode.SOURCEPACKAGES,
+      deploymentMode: deploymentMode,
       isRetryOnFailure: this.pool.retryOnFailure,
       artifacts: this.artifacts
     };
---
+++ packages/sfpowerscripts-cli/src/impl/prepare/SourceTrackingResource.ts
@@ -0,0 +1,142 @@
+import ScratchOrg from "@dxatscale/sfpowerscripts.core/src/scratchorg/ScratchOrg";
+const path = require("path");
+import * as fs from "fs-extra";
+import lodash = require("lodash");
+import AdmZip = require("adm-zip");
+import child_process = require("child_process");
+import { DeploymentResult } from "../deploy/DeployImpl";
+import SFPLogger, { Logger } from "@dxatscale/sfpowerscripts.core/lib/logger/SFPLogger";
+
+export default class SourceTrackingResource {
+  private readonly aggregatedSourceTrackingDir = ".sfpowerscripts/sourceTrackingFiles";
+  private readonly aggregatedUsernameDir = path.join(this.aggregatedSourceTrackingDir, this.scratchOrg.username);
+  private readonly aggregatedMaxRevisionFilePath = path.join(this.aggregatedUsernameDir, "maxRevision.json");
+  private readonly aggregatedSourcePathinfosFilePath = path.join(this.aggregatedUsernameDir, "sourcePathInfos.json");
+
+  constructor(
+    private scratchOrg: ScratchOrg,
+    private deploymentResult: DeploymentResult,
+    private logger: Logger
+  ) {
+    fs.mkdirpSync(this.aggregatedUsernameDir);
+
+    this.aggregateSourceTrackingResources();
+  }
+
+  /**
+   * Deploy source tracking resource to scratch org as a static resource
+   */
+  deploy(): void {
+    // Deploy static resource to SO
+    let projectConfig = {
+      packageDirectories: [
+        {
+          path: "force-app",
+          default: true
+        }
+      ],
+      namespace: "",
+      sourceApiVersion: "49.0"
+    };
+
+    fs.writeJSONSync(path.join(this.aggregatedUsernameDir, "sfdx-project.json"), projectConfig, { spaces: 2 });
+    let staticResourcesDir = path.join(this.aggregatedUsernameDir, "force-app", "main", "default", "staticresources");
+    fs.mkdirpSync(staticResourcesDir);
+
+    let zip = new AdmZip();
+    zip.addLocalFile(this.aggregatedMaxRevisionFilePath);
+    zip.addLocalFile(this.aggregatedSourcePathinfosFilePath);
+    zip.writeZip(path.join(staticResourcesDir, "sourceTrackingFiles.zip"));
+
+    let metadataXml: string =
+      `<?xml version="1.0" encoding="UTF-8"?>
+      <StaticResource xmlns="http://soap.sforce.com/2006/04/metadata">
+        <cacheControl>Public</cacheControl>
+        <contentType>application/zip</contentType>
+      </StaticResource>`;
+
+    fs.writeFileSync(path.join(staticResourcesDir, "sourceTrackingFiles.resource-meta.xml"), metadataXml);
+
+    try {
+      child_process.execSync(
+        `sfdx force:source:deploy -p force-app -u ${this.scratchOrg.username}`,
+        {
+          cwd: this.aggregatedUsernameDir,
+          encoding: 'utf8',
+          stdio: 'pipe'
+        }
+      );
+    } catch (error) {
+      SFPLogger.log(
+        `Failed to deploy static resources to scratch org`,
+        null,
+        this.logger
+      );
+      throw error;
+    }
+  }
+
+  /**
+   * Aggregate source tracking resources, maxRevision.json and sourcePathInfos.json, across artifacts, for scratch org username
+   */
+  private aggregateSourceTrackingResources() {
+    for (let packageInfoOfDeployedArtifact of this.deploymentResult.deployed) {
+      console.log(packageInfoOfDeployedArtifact.sourceDirectory);
+      let orgsDir = path.join(packageInfoOfDeployedArtifact.sourceDirectory, ".sfdx", "orgs");
+      let usernameDir = path.join(orgsDir, this.scratchOrg.username);
+
+      if (!fs.existsSync(usernameDir))
+        throw new Error(`Failed to consolidate source tracking files. Unable to find ${usernameDir}`);
+
+      let maxRevisionFilePath = path.join(usernameDir, "maxRevision.json");
+      let sourcePathInfosFilePath = path.join(usernameDir, "sourcePathInfos.json");
+
+      if (!fs.existsSync(maxRevisionFilePath) || !fs.existsSync(sourcePathInfosFilePath))
+        throw new Error(`Failed to consolidate source tracking files. Missing source tracking files`);
+
+      if (fs.existsSync(this.aggregatedMaxRevisionFilePath) && fs.existsSync(this.aggregatedSourcePathinfosFilePath)) {
+
+        let aggregatedMaxRevision = fs.readJSONSync(this.aggregatedMaxRevisionFilePath, { encoding: "UTF-8" });
+        let maxRevision = fs.readJSONSync(maxRevisionFilePath, { encoding: "UTF-8" });
+        if (maxRevision.serverMaxRevisionCounter >= aggregatedMaxRevision.serverMaxRevisionCounter) {
+          // Replace maxRevision.json
+          fs.copySync(maxRevisionFilePath, this.aggregatedMaxRevisionFilePath, { overwrite: true });
+        }
+
+        // Concatenate sourcePathInfos.json
+        let aggregatedSourcePathInfos = fs.readJSONSync(this.aggregatedSourcePathinfosFilePath, { encoding: "UTF-8" });
+        let sourcePathInfos = fs.readJSONSync(sourcePathInfosFilePath, { encoding: "UTF-8" });
+
+        this.truncateSourcePathInfos(sourcePathInfos, packageInfoOfDeployedArtifact.sourceDirectory);
+
+        Object.assign(aggregatedSourcePathInfos, sourcePathInfos);
+
+        fs.writeJSONSync(this.aggregatedSourcePathinfosFilePath, aggregatedSourcePathInfos, { spaces: 2 });
+      } else {
+        fs.copySync(maxRevisionFilePath, this.aggregatedMaxRevisionFilePath);
+        fs.copySync(sourcePathInfosFilePath, this.aggregatedSourcePathinfosFilePath);
+
+        let aggregatedSourcePathInfos = fs.readJSONSync(this.aggregatedSourcePathinfosFilePath, { encoding: "UTF-8" });
+        this.truncateSourcePathInfos(aggregatedSourcePathInfos, packageInfoOfDeployedArtifact.sourceDirectory);
+
+        fs.writeJSONSync(this.aggregatedSourcePathinfosFilePath, aggregatedSourcePathInfos, { spaces: 2 });
+      }
+    }
+  }
+
+  /**
+   * Truncate Source Path Infos by removing source directory and keeping just the package directory
+   * @param sourcePathInfos
+   * @param sourceDirectory
+   */
+  private truncateSourcePathInfos(sourcePathInfos: any, sourceDirectory: string) {
+    for (let entry of Object.entries<any>(sourcePathInfos)) {
+      let newPropName = entry[0].replace(path.resolve(sourceDirectory), "");
+      let newPropValue = lodash.cloneDeep(entry[1]);
+      newPropValue.sourcePath = newPropValue.sourcePath.replace(path.resolve(sourceDirectory), "");
+      sourcePathInfos[newPropName] = newPropValue;
+
+      delete sourcePathInfos[entry[0]];
+    }
+  }
+}
