7e3f56d97d3d71f220e3938180e0e75fdfd75b9c
Alan Ly <alan.ly@accenture.com>
2021-07-12 12:02:21 +1000

    Output logs for push source deployment type
    - Metadata deployment table
    - Component failures table

---
+++ packages/core/src/display/PushErrorDisplayer.ts
@@ -0,0 +1,39 @@
+const Table = require("cli-table");
+import SFPLogger, { Logger, LoggerLevel } from "../logger/SFPLogger";
+
+export default class DeployErrorDisplayer {
+
+  public static printMetadataFailedToPush(componentFailures: any, packageLogger: Logger) {
+
+    if (componentFailures === null || componentFailures === undefined) return;
+
+    let table = new Table({
+      head: ["Metadata Type", "API Name","Problem Type","Problem"],
+    });
+
+    let pushComponentFailureIntoTable = (componentFailure) => {
+        let item = [
+          componentFailure.type,
+          componentFailure.fullName,
+          componentFailure.problemType,
+          componentFailure.error
+        ];
+        table.push(item);
+    };
+
+    if (componentFailures instanceof Array) {
+      for (let failure of componentFailures) {
+        pushComponentFailureIntoTable(failure);
+      }
+    } else {
+      let failure = componentFailures;
+      pushComponentFailureIntoTable(failure);
+    }
+    SFPLogger.log(
+      "The following components resulted in failures:",
+      LoggerLevel.ERROR,
+      packageLogger
+    );
+    SFPLogger.log(table.toString(), LoggerLevel.ERROR,packageLogger);
+  }
+}
