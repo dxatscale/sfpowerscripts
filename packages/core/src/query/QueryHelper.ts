d7b934a4fbc10bd2996fb3c679b85112f354f9a5
Alan Ly <36793262+aly76@users.noreply.github.com>
2021-07-15 17:21:04 +1000

    Display contents of scratch org on fetch (#602)
    * Display contents of scratch org on fetch
    
    * Move displayOrgContents to fetch command layer
    
    Co-authored-by: Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>

---
+++ packages/core/src/query/QueryHelper.ts
@@ -0,0 +1,23 @@
+import { Connection } from "@salesforce/core";
+const retry = require("async-retry");
+
+
+
+export default class QueryHelper {
+
+
+  static async query(query: string, conn: Connection, isTooling: boolean) {
+    return await retry(
+      async (bail) => {
+        let records;
+        if (isTooling)
+          records = (await conn.tooling.query<any>(query)).records;
+        else
+          records = (await conn.query<any>(query)).records;
+
+        return records;
+      },
+      { retries: 3, minTimeout: 2000 }
+    );
+  }
+}
