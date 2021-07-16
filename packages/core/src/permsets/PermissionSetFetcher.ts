7ed9b98e0021b9602fbb58d48f20f65931934f54
Alan Ly <alan.ly@accenture.com>
2021-07-16 14:50:17 +1000

    Use QueryHelper in PermissionSetFetcher

--- packages/core/src/permsets/PermissionSetFetcher.ts
+++ packages/core/src/permsets/PermissionSetFetcher.ts
@@ -1,22 +1,18 @@
 import { Connection } from "@salesforce/core";
+import QueryHelper from "../query/QueryHelper";
 const retry = require("async-retry");
 
 /*
  * Retrieve Permsets for a user from a target org
  */
 export default class PermissionSetFetcher {
+
   constructor(private username: string, private conn: Connection) {}
+
   public async fetchAllPermsetAssignment() {
 
+    const query = `SELECT Id, PermissionSet.Name, Assignee.Username FROM PermissionSetAssignment WHERE Assignee.Username = '${this.username}'`;
 
-    return retry(
-      async (bail) => {
-        //Find all permsets assigned to this user
-        let query = `SELECT Id, PermissionSet.Name, Assignee.Username FROM PermissionSetAssignment WHERE Assignee.Username = '${this.username}'`;
-        const result = await this.conn.query(query);
-        return result.records
-      },
-      { retries: 3, minTimeout: 3000 }
-    );
+    return await QueryHelper.query<any>(query, this.conn, false);
   }
 }
