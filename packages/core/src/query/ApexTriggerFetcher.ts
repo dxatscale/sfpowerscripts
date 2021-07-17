d1393b019d858f94726b810c1eb00ec62a4c01ea
Alan Ly <alan.ly@accenture.com>
2021-07-17 14:54:35 +1000

    Refactor PackageTestCoverage
    Move ApexClass, ApexTrigger and ApexCodeCoverageAggregate queries into own
    class.
    
    Add test case for calculating package coverage with untouched classes and
    triggers.

---
+++ packages/core/src/query/ApexTriggerFetcher.ts
@@ -0,0 +1,25 @@
+import { Connection } from "@salesforce/core";
+import QueryHelper from "./QueryHelper";
+
+export default class ApexTriggerFetcher {
+
+  constructor(
+    private conn: Connection
+  ) {}
+
+  /**
+   * Query Triggers by Name
+   *
+   * @param triggerNames
+   * @returns
+   */
+  public async fetchApexTriggerByName(
+    triggerNames: string[]
+  ): Promise<{ Id: string; Name: string; }[]> {
+
+    let collection = triggerNames.map((name) => `'${name}'`).toString(); // transform into formatted string for query
+    let query = `SELECT ID, Name FROM ApexTrigger WHERE Name IN (${collection})`;
+
+    return await QueryHelper.query<{ Id: string; Name: string; }>(query, this.conn, false);
+  }
+}
