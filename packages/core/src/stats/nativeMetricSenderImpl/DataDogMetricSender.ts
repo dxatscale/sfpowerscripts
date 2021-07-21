8b524cab60d728797849de6e9529f64dda3e01db
Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>
2021-07-21 11:16:39 +1000

    Add New Relic Integration (#610)
    Co-authored-by: azlam-abdulsalam <sfpowerscripts@dxscale>

---
+++ packages/core/src/stats/nativeMetricSenderImpl/DataDogMetricSender.ts
@@ -0,0 +1,68 @@
+import { BufferedMetricsLogger } from "datadog-metrics";
+import SFPLogger, { Logger, LoggerLevel } from "../../logger/SFPLogger";
+import { NativeMetricSender } from "../NativeMetricSender";
+
+
+export class DataDogMetricsSender extends NativeMetricSender
+{
+
+  constructor(logger:Logger)
+  {
+    super(logger);
+  }
+  
+  
+  private  nativeDataDogMetricsLogger: BufferedMetricsLogger;
+
+  public initialize(apiHost: string, apiKey: string) {
+    try {
+      this.nativeDataDogMetricsLogger = new BufferedMetricsLogger({
+        apiHost: apiHost,
+        apiKey: apiKey,
+        prefix: "sfpowerscripts.",
+        flushIntervalSeconds: 0,
+      });
+    } catch (error) {
+      SFPLogger.log("Unable to intialize native datadog logger"+error,LoggerLevel.TRACE,this.logger);
+    }
+  }
+  
+  public  sendGaugeMetric(
+    metric: string,
+    value: number,
+    tags: string[] | { [key: string]: string }
+  ) {
+    try {
+      let transformedTags = this.transformTagsToStringArray(tags);
+      this.nativeDataDogMetricsLogger.gauge(
+        metric,
+        value,
+        transformedTags
+      );
+      this.nativeDataDogMetricsLogger.flush();
+    } catch (error) {
+      SFPLogger.log(`Unable to transmit metrics for metric ${metric} due to`+error,LoggerLevel.TRACE,this.logger);
+    }
+  }
+
+
+  public sendCountMetric(
+    metric: string,
+    tags: string[] | { [key: string]: string }
+  ) {
+    try {
+      let transformedTags = this.transformTagsToStringArray(tags);
+      this.nativeDataDogMetricsLogger.increment(
+        metric,
+        1,
+        transformedTags
+      );
+      this.nativeDataDogMetricsLogger.flush();
+    } catch (error) {
+      SFPLogger.log(`Unable to transmit metrics for metric ${metric} due to`+error,LoggerLevel.TRACE,this.logger);
+    }
+  }
+
+ 
+
+}
