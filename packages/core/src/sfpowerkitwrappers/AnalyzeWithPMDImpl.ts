e17085f565bba13c45073f27f2e3d4c689d6c8b6
azlam-abdulsalam <sfpowerscripts@dxscale>
2021-08-10 15:47:51 +1000

    Fix Issue with working directory

--- packages/core/src/sfpowerkitwrappers/AnalyzeWithPMDImpl.ts
+++ packages/core/src/sfpowerkitwrappers/AnalyzeWithPMDImpl.ts
@@ -11,7 +11,7 @@ export default class AnalyzeWithPMDImpl extends SFDXCommand {
     protected logger?: Logger,
     protected logLevel?: LoggerLevel
   ) {
-    super(null, sourceDirectory,logger,logLevel);
+    super(null, null,logger,logLevel);
   }
 
   getSFDXCommand(): string {
