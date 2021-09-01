4504de702db31fdf505a3e87a70c17930afe8ee0
Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>
2021-09-02 09:35:50 +1000

    Fix #626 Improve Create Package output to match orchestrator (#672)
    * Fix #626 Improve Create Package output to match orchestrator
    
    * Fix Deepscan Lint Issues
    
    * Fix Missing import statments
    
    * Abstract Create package to a common command
    
    Co-authored-by: azlam-abdulsalam <sfpowerscripts@dxscale>

---
+++ packages/sfpowerscripts-cli/src/PackageCreateCommand.ts
@@ -0,0 +1,284 @@
+import ArtifactGenerator from "@dxatscale/sfpowerscripts.core/lib/generators/ArtifactGenerator";
+import { COLOR_HEADER, COLOR_KEY_MESSAGE, ConsoleLogger } from "@dxatscale/sfpowerscripts.core/lib/logger/SFPLogger";
+import PackageDiffImpl from "@dxatscale/sfpowerscripts.core/lib/package/PackageDiffImpl";
+import PackageMetadata from "@dxatscale/sfpowerscripts.core/lib/PackageMetadata";
+import { flags } from "@salesforce/command";
+import { fs, Messages } from "@salesforce/core";
+import { execSync } from "child_process";
+import { EOL } from "os";
+import SfpowerscriptsCommand from "./SfpowerscriptsCommand";
+
+
+
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('@dxatscale/sfpowerscripts', 'create-package');
+
+
+export default abstract class PackageCreateCommand extends SfpowerscriptsCommand {
+
+  protected static requiresUsername = false;
+  protected static requiresDevhubUsername = false;
+  protected static requiresProject = true;
+
+
+  protected static flagsConfig = {
+    package: flags.string({
+      required: true,
+      char: "n",
+      description: messages.getMessage("packageFlagDescription"),
+    }),
+    diffcheck: flags.boolean({
+      description: messages.getMessage("diffCheckFlagDescription"),
+    }),
+    gittag: flags.boolean({
+      description: messages.getMessage("gitTagFlagDescription"),
+    }),
+    repourl: flags.string({
+      char: "r",
+      description: messages.getMessage("repoUrlFlagDescription"),
+    }),
+    versionnumber: flags.string({
+      description: messages.getMessage("versionNumberFlagDescription"),
+    }),
+    artifactdir: flags.directory({
+      description: messages.getMessage("artifactDirectoryFlagDescription"),
+      default: "artifacts",
+    }),
+    branch:flags.string({
+      description:messages.getMessage("branchFlagDescription"),
+    }),
+    refname: flags.string({
+      description: messages.getMessage("refNameFlagDescription"),
+    }),
+    loglevel: flags.enum({
+      description: "logging level for this command invocation",
+      default: "info",
+      required: false,
+      options: [
+        "trace",
+        "debug",
+        "info",
+        "warn",
+        "error",
+        "fatal",
+        "TRACE",
+        "DEBUG",
+        "INFO",
+        "WARN",
+        "ERROR",
+        "FATAL",
+      ],
+    })
+  };
+
+
+  protected sfdxPackage: string;
+  protected versionNumber: string;
+  protected artifactDirectory: string;
+  protected refname: string; 
+  protected branch:string;
+  protected commitId:string
+  protected repositoryURL:string;
+ 
+    /**
+   * Entry point for package installation commands
+   *
+   */
+   async execute(): Promise<any> {
+      let isToCreatePackage = await this.preCreate();
+      if(isToCreatePackage)
+      {
+        try
+        {
+         let packageMetadata = await this.create();
+         await this.postCreate(packageMetadata);
+        }catch(err)
+        {
+          console.log(err);
+          process.exit(1);
+        }
+        
+         
+      }
+    }
+ 
+
+
+
+  private async preCreate():Promise<boolean>
+  {
+     this.sfdxPackage = this.flags.package;
+     this.versionNumber = this.flags.versionnumber;
+     this.artifactDirectory = this.flags.artifactdir;
+     this.refname = this.flags.refname;
+     this.branch=this.flags.branch;
+
+
+     if(this.hubOrg)
+       await this.hubOrg.refreshAuth();
+
+    let isToRunBuild;
+
+     if (this.flags.diffcheck) {
+       let packageDiffImpl = new PackageDiffImpl(
+         new ConsoleLogger(),
+         this.sfdxPackage,
+         null,
+         this.getConfigFilePath()
+       );
+
+       let isToRunBuild = (await packageDiffImpl.exec()).isToBeBuilt;
+
+       if (isToRunBuild)
+         console.log(
+           `Detected changes to ${this.sfdxPackage} package...proceeding\n`
+         );
+       else
+         console.log(
+           `No changes detected for ${this.sfdxPackage} package...skipping\n`
+         );
+     } else isToRunBuild = true;
+
+     if (isToRunBuild) {
+       if (this.flags.repourl == null) {
+         this.repositoryURL = execSync("git config --get remote.origin.url", { encoding: "utf8",stdio:"pipe"});
+         // Remove new line '\n' from end of url
+         this.repositoryURL = this.repositoryURL.slice(0, this.repositoryURL.length - 1);
+       } else this.repositoryURL = this.flags.repourl;
+
+        this.commitId = execSync("git log --pretty=format:%H -n 1", { encoding: "utf8",stdio:"pipe"});
+      }
+    return isToRunBuild;
+
+  }
+  
+  
+  protected abstract getConfigFilePath():string;
+
+  protected abstract create():Promise<PackageMetadata>;
+
+
+  private async  postCreate(packageMetadata:PackageMetadata) {
+        this.printPackageDetails(packageMetadata);
+
+        if (this.flags.gittag) {
+
+          execSync(`git config --global user.email "sfpowerscripts@dxscale.io"`);
+          execSync(`git config --global user.name "sfpowerscripts"`);
+
+          let tagname = `${this.sfdxPackage}_v${packageMetadata.package_version_number}`;
+          console.log(`Creating tag ${tagname}`);
+          execSync(
+            `git tag -a -m "${packageMetadata.package_name} sfpowerscripts package ${packageMetadata.package_version_number}" ${tagname} HEAD`,
+            { encoding: "utf8",stdio:"pipe"}
+          );
+
+          packageMetadata.tag = tagname;
+        }
+
+        //Generate Artifact
+        let artifactFilepath: string = await ArtifactGenerator.generateArtifact(
+          this.sfdxPackage,
+          process.cwd(),
+          this.artifactDirectory,
+          packageMetadata
+        );
+
+
+       this.generateEnvironmentVariables(artifactFilepath, packageMetadata);
+        
+      
+  }
+  
+  private generateEnvironmentVariables(artifactFilepath: string, packageMetadata: PackageMetadata) {
+    let prefix = "sfpowerscripts";
+    if (this.refname != null)
+      prefix = `${this.refname}_${prefix}`;
+
+    console.log("\nOutput variables:");
+
+
+    fs.writeFileSync(
+      ".env",
+      `${prefix}_artifact_directory=${artifactFilepath}\n`,
+      { flag: "a" }
+    );
+    console.log(
+      `${prefix}_artifact_directory=${artifactFilepath}`
+    );
+    fs.writeFileSync(
+      ".env",
+      `${prefix}_package_version_number=${packageMetadata.package_version_number}\n`,
+      { flag: "a" }
+    );
+    console.log(
+      `${prefix}_package_version_number=${packageMetadata.package_version_number}`
+    );
+
+
+    if (packageMetadata.package_version_id) {
+      fs.writeFileSync(
+        ".env",
+        `${prefix}_package_version_id=${packageMetadata.package_version_id}\n`,
+        { flag: "a" }
+      );
+      console.log(
+        `${prefix}_package_version_id=${packageMetadata.package_version_id}`
+      );
+    }
+  }
+
+  protected printPackageDetails(packageMetadata: PackageMetadata) {
+    console.log(
+      COLOR_HEADER(`${EOL}${
+        packageMetadata.package_name
+      } package created in ${this.getFormattedTime(
+        packageMetadata.creation_details.creation_time
+      )}`
+    ));
+    console.log(COLOR_HEADER(`-- Package Details:--`));
+    console.log(
+      COLOR_HEADER(`-- Package Version Number:        `),
+      COLOR_KEY_MESSAGE(packageMetadata.package_version_number)
+    );
+
+    if (packageMetadata.package_type !== "data") {
+      if (packageMetadata.package_type == "unlocked") {
+        console.log(
+          COLOR_HEADER(`-- Package Version Id:             `),
+          COLOR_KEY_MESSAGE(packageMetadata.package_version_id)
+        );
+        console.log(
+          COLOR_HEADER(`-- Package Test Coverage:          `),
+          COLOR_KEY_MESSAGE(packageMetadata.test_coverage)
+        );
+        console.log(
+          COLOR_HEADER(`-- Package Coverage Check Passed:  `),
+          COLOR_KEY_MESSAGE(packageMetadata.has_passed_coverage_check)
+        );
+      }
+
+      console.log(
+        COLOR_HEADER(`-- Apex In Package:             `),
+        COLOR_KEY_MESSAGE(packageMetadata.isApexFound ? "Yes" : "No")
+      );
+      console.log(
+        COLOR_HEADER(`-- Profiles In Package:         `),
+        COLOR_KEY_MESSAGE(packageMetadata.isProfilesFound ? "Yes" : "No")
+      );
+      console.log(
+        COLOR_HEADER(`-- Metadata Count:         `),
+        COLOR_KEY_MESSAGE(packageMetadata.metadataCount)
+      );
+    }
+  }
+  
+
+  protected getFormattedTime(milliseconds: number): string {
+    let date = new Date(0);
+    date.setSeconds(milliseconds / 1000); // specify value for SECONDS here
+    let timeString = date.toISOString().substr(11, 8);
+    return timeString;
+  }
+
+}
--- packages/sfpowerscripts-cli/src/commands/sfpowerscripts/package/data/create.ts
+++ packages/sfpowerscripts-cli/src/commands/sfpowerscripts/package/data/create.ts
@@ -1,22 +1,23 @@
-import { flags } from '@salesforce/command';
-import SfpowerscriptsCommand from "../../../../SfpowerscriptsCommand"
-import { Messages } from '@salesforce/core';
-import PackageDiffImpl from '@dxatscale/sfpowerscripts.core/lib/package/PackageDiffImpl';
+import { flags } from "@salesforce/command";
+import { Messages } from "@salesforce/core";
 import PackageMetadata from "@dxatscale/sfpowerscripts.core/lib/PackageMetadata";
-import ArtifactGenerator from "@dxatscale/sfpowerscripts.core/lib/generators/ArtifactGenerator";
-import { exec } from "shelljs";
-import * as fs from "fs-extra";
-import path = require("path");
-import ProjectConfig from '@dxatscale/sfpowerscripts.core/lib/project/ProjectConfig';
-import CreateDataPackageImpl from '@dxatscale/sfpowerscripts.core/lib/sfpcommands/package/CreateDataPackageImpl';
-import { ConsoleLogger } from '@dxatscale/sfpowerscripts.core/lib/logger/SFPLogger';
+
+import ProjectConfig from "@dxatscale/sfpowerscripts.core/lib/project/ProjectConfig";
+import CreateDataPackageImpl from "@dxatscale/sfpowerscripts.core/lib/sfpcommands/package/CreateDataPackageImpl";
+import {
+  COLOR_SUCCESS,
+  ConsoleLogger,
+} from "@dxatscale/sfpowerscripts.core/lib/logger/SFPLogger";
+import PackageCreateCommand from "../../../../PackageCreateCommand";
 
 Messages.importMessagesDirectory(__dirname);
-const messages = Messages.loadMessages('@dxatscale/sfpowerscripts', 'create_data_package');
+const messages = Messages.loadMessages(
+  "@dxatscale/sfpowerscripts",
+  "create_data_package"
+);
 
-export default class CreateDataPackage extends SfpowerscriptsCommand {
-
-  public static description = messages.getMessage('commandDescription');
+export default class CreateDataPackage extends PackageCreateCommand {
+  public static description = messages.getMessage("commandDescription");
 
   public static examples = [
     `$ sfdx sfpowerscripts:package:data:create -n mypackage -v <version>`,
@@ -25,24 +26,40 @@ export default class CreateDataPackage extends SfpowerscriptsCommand {
     `sfpowerscripts_artifact_directory`,
     `<refname>_sfpowerscripts_artifact_directory`,
     `sfpowerscripts_package_version_number`,
-    `<refname>_sfpowerscripts_package_version_number`
+    `<refname>_sfpowerscripts_package_version_number`,
   ];
 
-  protected static requiresUsername = false;
-  protected static requiresDevhubUsername = false;
-  protected static requiresProject = true;
-
   protected static flagsConfig = {
-    package: flags.string({required: true, char: 'n', description: messages.getMessage('packageFlagDescription')}),
-    versionnumber: flags.string({required: true, char: 'v', description: messages.getMessage('versionNumberFlagDescription')}),
-    artifactdir: flags.directory({description: messages.getMessage('artifactDirectoryFlagDescription'), default: 'artifacts'}),
-    diffcheck: flags.boolean({description: messages.getMessage('diffCheckFlagDescription')}),
-    branch:flags.string({
-      description:messages.getMessage("branchFlagDescription"),
+    package: flags.string({
+      required: true,
+      char: "n",
+      description: messages.getMessage("packageFlagDescription"),
     }),
-    gittag: flags.boolean({description: messages.getMessage('gitTagFlagDescription')}),
-    repourl: flags.string({char: 'r', description: messages.getMessage('repoUrlFlagDescription')}),
-    refname: flags.string({description: messages.getMessage('refNameFlagDescription')}),
+    versionnumber: flags.string({
+      required: true,
+      char: "v",
+      description: messages.getMessage("versionNumberFlagDescription"),
+    }),
+    artifactdir: flags.directory({
+      description: messages.getMessage("artifactDirectoryFlagDescription"),
+      default: "artifacts",
+    }),
+    diffcheck: flags.boolean({
+      description: messages.getMessage("diffCheckFlagDescription"),
+    }),
+    branch: flags.string({
+      description: messages.getMessage("branchFlagDescription"),
+    }),
+    gittag: flags.boolean({
+      description: messages.getMessage("gitTagFlagDescription"),
+    }),
+    repourl: flags.string({
+      char: "r",
+      description: messages.getMessage("repoUrlFlagDescription"),
+    }),
+    refname: flags.string({
+      description: messages.getMessage("refNameFlagDescription"),
+    }),
     loglevel: flags.enum({
       description: "logging level for this command invocation",
       default: "info",
@@ -61,111 +78,44 @@ export default class CreateDataPackage extends SfpowerscriptsCommand {
         "ERROR",
         "FATAL",
       ],
-    })
+    }),
   };
 
-
-  public async execute(){
-    try {
-
-      const sfdx_package: string = this.flags.package;
-      const version_number: string = this.flags.versionnumber;
-      const artifactDirectory: string = this.flags.artifactdir;
-      const refname: string = this.flags.refname;
-      const branch:string=this.flags.branch;
-
-      let packageDescriptor = ProjectConfig.getSFDXPackageDescriptor(null, sfdx_package);
-      if (packageDescriptor.type?.toLowerCase() !== "data") {
-        throw new Error("Data packages must have 'type' property of 'data' defined in sfdx-project.json");
-      }
-
-      let runBuild: boolean;
-      if (this.flags.diffcheck) {
-        let packageDiffImpl = new PackageDiffImpl(new ConsoleLogger(),sfdx_package, null);
-
-        runBuild = (await packageDiffImpl.exec()).isToBeBuilt;
-
-        if ( runBuild )
-        console.log(`Detected changes to ${sfdx_package} package...proceeding\n`);
-        else
-        console.log(`No changes detected for ${sfdx_package} package...skipping\n`);
-
-      } else runBuild = true;
-
-      if (runBuild) {
-        let commit_id = exec('git log --pretty=format:%H -n 1', {silent:true}).stdout;
-
-        let repository_url: string;
-        if (this.flags.repourl == null) {
-          repository_url = exec('git config --get remote.origin.url', {silent:true});
-          // Remove new line '\n' from end of url
-          repository_url = repository_url.slice(0,repository_url.length - 1);
-        } else repository_url = this.flags.repourl;
-
-
-
-
-        let packageMetadata:PackageMetadata = {
-          package_name: sfdx_package,
-          package_version_number: version_number,
-          sourceVersion: commit_id,
-          repository_url:repository_url,
-          branch:branch
-        };
-
-
-        let createDataPackageImpl = new CreateDataPackageImpl(
-          null,
-          sfdx_package,
-          packageMetadata,
-          false,
-          new ConsoleLogger()
-        );
-        packageMetadata = await createDataPackageImpl.exec();
-
-        if (this.flags.gittag) {
-          exec(`git config --global user.email "sfpowerscripts@dxscale"`);
-          exec(`git config --global user.name "sfpowerscripts"`);
-          let tagname = `${sfdx_package}_v${version_number}`;
-
-          console.log(`Creating tag ${tagname}`);
-          exec(`git tag -a -m "${sfdx_package} Data Package ${version_number}" ${tagname} HEAD`, {silent:false});
-
-          packageMetadata.tag = tagname;
-        }
-
-        console.log(JSON.stringify(packageMetadata));
-
-
-       //Generate Artifact
-        let artifactFilepath: string = await ArtifactGenerator.generateArtifact(
-          sfdx_package,
-          process.cwd(),
-          artifactDirectory,
-          packageMetadata
-        );
-
-        console.log(`Created data package ${path.basename(artifactFilepath)}`);
-
-
-
-        console.log("\nOutput variables:");
-        if (refname != null) {
-          fs.writeFileSync('.env', `${refname}_sfpowerscripts_artifact_directory=${artifactFilepath}\n`, {flag:'a'});
-          console.log(`${refname}_sfpowerscripts_artifact_directory=${artifactFilepath}`);
-          fs.writeFileSync('.env', `${refname}_sfpowerscripts_package_version_number=${version_number}\n`, {flag:'a'});
-          console.log(`${refname}_sfpowerscripts_package_version_number=${version_number}`);
-        } else {
-          fs.writeFileSync('.env', `sfpowerscripts_artifact_directory=${artifactFilepath}\n`, {flag:'a'});
-          console.log(`sfpowerscripts_artifact_directory=${artifactFilepath}`);
-          fs.writeFileSync('.env', `sfpowerscripts_package_version_number=${version_number}\n`, {flag:'a'});
-          console.log(`sfpowerscripts_package_version_number=${version_number}`);
-        }
-      }
-    } catch (err) {
-      console.log(err.message);
-      // Fail the task when an error occurs
-      process.exit(1);
+  protected async create(): Promise<PackageMetadata> {
+    let packageDescriptor = ProjectConfig.getSFDXPackageDescriptor(
+      null,
+      this.sfdxPackage
+    );
+    if (packageDescriptor.type?.toLowerCase() !== "data") {
+      throw new Error(
+        "Data packages must have 'type' property of 'data' defined in sfdx-project.json"
+      );
     }
+
+    let packageMetadata: PackageMetadata = {
+      package_name: this.sfdxPackage,
+      package_version_number: this.versionNumber,
+      sourceVersion: this.commitId,
+      repository_url: this.repositoryURL,
+      branch: this.branch,
+    };
+
+    let createDataPackageImpl = new CreateDataPackageImpl(
+      null,
+      this.sfdxPackage,
+      packageMetadata,
+      false,
+      new ConsoleLogger()
+    );
+    packageMetadata = await createDataPackageImpl.exec();
+
+    console.log(
+      COLOR_SUCCESS(`Created data package ${packageMetadata.package_name}`)
+    );
+    return packageMetadata;
+  }
+
+  protected getConfigFilePath(): string {
+    return null;
   }
 }
