3c1823fe60a7e6bcec4c4b193dd7a7a7a8fb3f73
Caitlyn Mills <63215232+Caitlyn-Mills@users.noreply.github.com>
2021-09-09 16:20:48 +0930

    Move functions to implementation class

--- packages/sfpowerscripts-cli/src/commands/sfpowerscripts/package/version/manage.ts
+++ packages/sfpowerscripts-cli/src/commands/sfpowerscripts/package/version/manage.ts
@@ -2,10 +2,8 @@ import { Messages } from "@salesforce/core";
 import SfpowerscriptsCommand from '../../../../SfpowerscriptsCommand';
 import { flags } from '@salesforce/command';
 import fs = require("fs");
-import inquirer = require("inquirer");
-import semver = require("semver");
-import Version from "@dxatscale/sfpowerscripts.core/lib/package/Version";
-import ProjectConfig from "@dxatscale/sfpowerscripts.core/lib/project/ProjectConfig"
+import VersionManageImpl from '/Users/caitlyn.mills/sfpowerscripts/sfpowerscripts/packages/sfpowerscripts-cli/src/impl/package/VersionManageImpl';
+
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
@@ -19,7 +17,11 @@ export default class Manage extends SfpowerscriptsCommand {
   public static description = messages.getMessage('commandDescription');
 
   public static examples = [
-    `sfdx sfpowerscripts:package:version:manage`
+    `sfdx sfpowerscripts:package:version:manage -a`,
+    `sfdx sfpowerscripts:package:version:manage -p package-name -b -v major -n -d`,
+    `sfdx sfpowerscripts:package:version:manage -allpackages --resetbuildnumber`,
+    `sfdx sfpowerscripts:package:version:manage -p package-name`
+
   ];
 
   protected static flagsConfig = {
@@ -50,6 +52,18 @@ export default class Manage extends SfpowerscriptsCommand {
         "patch",
       ]
     }),
+    noprompt: flags.boolean({
+      required: false,
+      default: false,
+      char: 'n',
+      description: "noPromptDescription"
+    }),
+    resetbuildnumber: flags.boolean({
+      required: false,
+      default: true,
+      char: 'b',
+      description: messages.getMessage("buildNumberDescription")
+    }),
     loglevel: flags.enum({
       description: "logging level for this command invocation",
       default: "info",
@@ -71,24 +85,6 @@ export default class Manage extends SfpowerscriptsCommand {
     })
   };
 
-
-  //TODO: no prompt 
-  //TODO: specific package - done, function in ProjectConfig class
-  //TODO: 
-  //TODO: Diff version - Talk alan 
-  //TODO: New class, dependent finder (list all dependents of a pcakage) - done, function in ProjectConfig Class
-
-  /**
-   * input a package: (parameter as a flag)
-   * If patch, minor - skip updating dependencies 
-   * If major, ask if dependencies need to be updated 
-   * Spit out list of dependents which were updated.
-   * Eg. The following packages were updated:: 
-   * 
-   */
-
-
-
   protected static requiresProject = true;
   protected static requiresUsername = false;
   protected static requiresDevhubUsername = false;
@@ -98,86 +94,18 @@ export default class Manage extends SfpowerscriptsCommand {
       const projectConfig = JSON.parse(
         fs.readFileSync("sfdx-project.json", "utf8")
       );
-      let packages = projectConfig.packageDirectories;
 
+      let versionManageImpl: VersionManageImpl = new VersionManageImpl(
+        projectConfig, 
+        this.flags.allpackages, 
+        this.flags.dependents, 
+        this.flags.package, 
+        this.flags.version, 
+        this.flags.noprompt, 
+        this.flags.resetbuildnumber
+      );
 
-
-      let version = new Version();
-      let dependencyMap = new Map();
-      let updatedPackages = new Map();
-
-      if (this.flags.allpackages) {
-        await this.updateAllWithInquirer(projectConfig);
-
-      } else {
-
-        //First iteration of packages to update version numbers 
-        for (let pkg of packages) {
-          console.log(`${pkg.package} version number: ${pkg.versionNumber}`)
-          let custom = false;
-          let skip = false;
-          await this.packagePrompt(pkg);
-
-          if (custom) {
-            pkg.versonNumber = await this.getCustom();
-          }
-
-
-          if (!skip) {
-            if (version.hasNonZeroBuildNo(pkg.versionNumber)) {
-              await inquirer.prompt([{ type: 'list', name: 'selection', message: `Would you like the build number for ${pkg.package} reset to 0?`, choices: ['Yes', 'No'] }]).then((answer) => {
-                if (answer.selection == 'Yes') { pkg.versionNumber = version.resetBuildNumber(pkg.versionNumber); }
-              });
-            }
-            updatedPackages.set(pkg.package, pkg.versionNumber);
-
-
-            if (!this.flags.dependencies) {
-              await inquirer.prompt([{ type: 'list', name: 'selection', message: `Would you like to update packages that are dependent on ${pkg.package}?`, choices: ['Yes', 'No'] }]).then((answer) => {
-                if (answer.selection == 'Yes') {
-                  let dependentPkgs = ProjectConfig.getDependents(pkg, projectConfig);
-                  if (dependentPkgs.length != 0) {
-                    dependentPkgs.forEach(dependent => {
-                      dependent = ProjectConfig.updateDependent(pkg, dependent);
-                    });
-
-                  }
-                }
-              });
-            } else {
-              let dependentPkgs = ProjectConfig.getDependents(pkg, projectConfig);
-              if (dependentPkgs.length != 0) {
-                dependentPkgs.forEach(dependent => {
-                  ProjectConfig.updateDependent(pkg, dependent);
-                });
-              }
-            }
-          }
-
-
-        }
-      }
-      console.log(`The following packages and dependencies will be updated in your sfdx-project.json file`);
-      for (let pkg of packages) {
-        if (updatedPackages.has(pkg.package)) {
-          console.log(`Package: ${pkg.package} Version Number: ${pkg.versionNumber}`);
-          if (dependencyMap.has(pkg.package)) {
-            console.log(`All packages with ${pkg.package} as a dependency will also be updated\n`);
-          } else {
-            console.log(`No packages with ${pkg.package} as a dependency will be updated\n`)
-          }
-        }
-      }
-      await inquirer.prompt([{ type: 'list', name: 'selection', message: `Would you like to write these package updates to the sfdx-project.json?`, choices: ['Yes, Update', 'No, Quit'] }]).then((answer) => {
-        if (answer.selection == 'Yes, Update') {
-          console.log('Updating sfdx-project.json')
-          let projectString = JSON.stringify(projectConfig, null, 4);
-          fs.writeFileSync('sfdx-project.json', projectString, 'utf-8');
-        } else {
-          console.log('Exiting command')
-          process.exit(1);
-        }
-      });
+      versionManageImpl.execute();
     }
     catch (err) {
       console.log(err);
@@ -185,130 +113,4 @@ export default class Manage extends SfpowerscriptsCommand {
     }
   }
 
-  /**
-   * prompt the user for the custom number when selected
-   * @returns 
-   */
-  private async getCustom() {
-    let customVersion;
-    await inquirer.prompt([{ type: 'input', name: 'selection', message: `Input custom versionNumber` }]).then((answers) => { customVersion = answers.selection; });
-    customVersion = Version.verifyCustom(customVersion)
-
-    if (!customVersion) {
-      console.log(`Custom number was invalid`);
-      return this.getCustom();
-    } else {
-      return customVersion;
-    }
-  }
-
-  /**
-   * Update a specific package inputted by the user with --package packagename flag 
-   * @param projectConfig 
-   */
-  private updatePackage(projectConfig) {
-    let packages = projectConfig.packageDirectories;
-
-  }
-
-
-  /**
-   * Iterate through all packages to ask the user what it would like to update
-   * @param projectConfig 
-   */
-  private async updateAllWithInquirer(projectConfig) {
-    let packages = projectConfig.packageDirectories;
-    for (let pkg of packages) {
-      let packagePrompt = await this.packagePrompt(pkg);
-      if (packagePrompt == "skip") {
-        console.log(`Skipped updating package version for ${pkg.package}`)
-      }
-      else if (packagePrompt == "custom") {
-        await this.getCustom();
-      } else {
-
-        if (this.flags.dependents) {
-          let dependentPkgs = ProjectConfig.getDependents(pkg.package, projectConfig);
-          if (dependentPkgs.length != 0) {
-            dependentPkgs.forEach(dependent => {
-              dependent = ProjectConfig.updateDependent(pkg, dependent);
-            });
-          }
-        } else {
-          await this.dependentPrompt(pkg, projectConfig);
-        }
-      }
-    }
-  }
-
-  /**
-   * Prompt using inquirier for the version of the given package to update
-   * @param pkg 
-   * @returns 
-   */
-  private async packagePrompt(pkg) {
-    let newMajor = Version.getMajor(pkg.versionNumber);
-    let newMinor = Version.getMinor(pkg.versionNumber);
-    let patch = Version.getPatch(pkg.versionNumber);
-    let skip;
-    let custom;
-
-    await inquirer.prompt([{ type: 'list', name: 'selection', message: `Would you like to update ${pkg.package}?`, choices: ['Major: ' + newMajor, 'Minor: ' + newMinor, 'Patch: ' + patch, 'Custom', 'Skip'] }]).then((selection) => {
-
-      /**If else statement based on the selection by the user */
-      if (selection.selection == 'Skip') {
-        return skip = true;
-      }
-      else if (selection.selection == 'Custom') {
-        return custom = true;
-      }
-      else if (selection.selection == 'Major: ' + newMajor) {
-        pkg.versionNumber = Version.update('major', pkg);
-      }
-      else if (selection.selection == 'Minor: ' + newMinor) {
-        pkg.versionNumber = Version.update('minor', pkg);
-      }
-      else if (selection.selection == 'Patch: ' + patch) {
-        pkg.versionNumber = Version.update('patch', pkg);;
-      }
-    });
-    if (skip == true) {
-      return 'skip'
-    }
-    if (custom == true) { return 'custom' }
-  }
-
-
-  /**
-   * Get all packages which have been updated
-   */
-  private getUpdated() { }
-
-  /**
-   * Write the updated config to the sfdx-project.json file
-   */
-  private static writeConfig() { }
-
-
-  /**
-   * Prompt the user on whether or not to update dependent packages
-   * @param pkg 
-   * @param projectConfig 
-   */
-  private async dependentPrompt(pkg, projectConfig) {
-    await inquirer.prompt([{ type: 'list', name: 'selection', message: `Would you like to update packages that are dependent on ${pkg.package}?`, choices: ['Yes', 'No'] }]).then((answer) => {
-      if (answer.selection == 'Yes') {
-        let dependentPkgs = ProjectConfig.getDependents(pkg.package, projectConfig);
-        if (dependentPkgs.length != 0) {
-          dependentPkgs.forEach(dependent => {
-            dependent = ProjectConfig.updateDependent(pkg, dependent);
-          });
-
-        }
-      }
-    });
-
-  }
-
-  private updateDependents() { }
 }
---
+++ packages/sfpowerscripts-cli/src/impl/package/VersionManageImpl.ts
@@ -0,0 +1,227 @@
+import inquirer = require("inquirer");
+import fs = require("fs");
+import Version from "@dxatscale/sfpowerscripts.core/lib/package/Version";
+import ProjectConfig from "@dxatscale/sfpowerscripts.core/lib/project/ProjectConfig"
+
+export default class VersionManageImpl {
+
+
+    protected projectConfig;
+    protected packageFlag;
+    protected packages;
+    protected allPackages;
+    protected dependents;
+    protected version;
+    protected noPrompt;
+    protected resetBuildNumber;
+
+    /**
+     * 
+     * @param projectConfig 
+     * @param allpackages 
+     * @param dependents 
+     * @param packageFlag 
+     */
+    constructor(projectConfig, allpackages, dependents, packageFlag, version, noprompt, resetbuildnumber) {
+
+        this.projectConfig = projectConfig;
+        this.packageFlag = packageFlag;
+        this.allPackages = allpackages;
+        this.packages = projectConfig.packageDirectories;
+        this.dependents = dependents;
+        this.version = version;
+        this.noPrompt = noprompt;
+        this.resetBuildNumber = resetbuildnumber;
+    }
+
+    public async execute(): Promise<any> {
+
+        let updatedPackages = new Map();
+
+        if (this.allPackages) {
+            updatedPackages = await this.updateAllWithInquirer();
+        }
+        else if (this.packageFlag != null) {
+            updatedPackages = await this.updatePackage();
+        }
+        else {
+            console.log('This command must be run with the --package flag or the --allpackages flag')
+            process.exit(1);
+        }
+        console.log(`\nThe following packages will be updated in your sfdx-project.json file`);
+        for (let pkg of this.packages) {
+            if (updatedPackages.has(pkg.package)) {
+                console.log(`Package: ${pkg.package} Version Number: ${pkg.versionNumber}`);
+            }
+        }
+        await this.writeConfig();
+
+    }
+    /**
+  * prompt the user for the custom number when selected. Verifies the custom number is valid
+  * @returns the customVersion if valid
+  */
+    private async getCustom() {
+        let customVersion;
+        await inquirer.prompt([{ type: 'input', name: 'selection', message: `Input custom versionNumber` }]).then((answers) => { customVersion = answers.selection; });
+        customVersion = Version.verifyCustom(customVersion)
+
+        if (!customVersion) {
+            console.log(`Custom number was invalid`);
+            return this.getCustom();
+        } else {
+            return customVersion;
+        }
+    }
+
+
+    private async updatePackage() {
+        let updatedPackages = new Map();
+        for (let pkg of this.packages) {
+            if (pkg.package == this.packageFlag) {
+                let packagePrompt;
+                if (this.version != null) {
+                    pkg.versionNumber = Version.update(this.version, pkg, this.resetBuildNumber);
+                    updatedPackages.set(pkg.package, pkg.versionNumber);
+                } else {
+                    packagePrompt = await this.packagePrompt(pkg);
+                    if (packagePrompt == "skip") {
+                        console.log(`Skipped updating package version for ${pkg.package}`)
+                    }
+                    else if (packagePrompt == "custom") {
+                        pkg.versionNumber = await this.getCustom();
+                        updatedPackages.set(pkg.package, pkg.versionNumber);
+                    } else {
+                        updatedPackages.set(pkg.package, pkg.versionNumber);
+                    }
+                }
+                if (this.dependents && packagePrompt != 'skip') {
+                    let dependentPkgs = ProjectConfig.getDependents(pkg.package, this.projectConfig);
+                    if (dependentPkgs.length != 0) {
+                        dependentPkgs.forEach(dependent => {
+                            dependent = ProjectConfig.updateDependent(pkg, dependent);
+                        });
+                    }
+                } else if (packagePrompt != 'skip') {
+                    await this.dependentPrompt(pkg);
+                }
+
+            }
+        }
+        return updatedPackages;
+    }
+
+
+    /**
+     * Iterate through all packages to ask the user what it would like to update
+     * @param projectConfig 
+     */
+    private async updateAllWithInquirer() {
+        let updatedPackages = new Map();
+        for (let pkg of this.packages) {
+            let packagePrompt = await this.packagePrompt(pkg);
+            if (packagePrompt == "skip") {
+                console.log(`Skipped updating package version for ${pkg.package}`)
+            }
+            else if (packagePrompt == "custom") {
+                pkg.versionNumber = await this.getCustom();
+                updatedPackages.set(pkg.package, pkg.versionNumber);
+            } else {
+                updatedPackages.set(pkg.package, pkg.versionNumber);
+            }
+
+            if (this.dependents && packagePrompt != 'skip') {
+                let dependentPkgs = ProjectConfig.getDependents(pkg.package, this.projectConfig);
+                if (dependentPkgs.length != 0) {
+                    dependentPkgs.forEach(dependent => {
+                        dependent = ProjectConfig.updateDependent(pkg, dependent);
+                    });
+                }
+            } else if (packagePrompt != 'skip') {
+                await this.dependentPrompt(pkg);
+            }
+        }
+        return updatedPackages;
+    }
+
+    /**
+     * Prompt using inquirier for the version of the given package to update
+     * @param pkg 
+     * @returns 
+     */
+    private async packagePrompt(pkg) {
+        let newMajor = Version.getMajor(pkg.versionNumber);
+        let newMinor = Version.getMinor(pkg.versionNumber);
+        let patch = Version.getPatch(pkg.versionNumber);
+        let skip;
+        let custom;
+
+        await inquirer.prompt([{ type: 'list', name: 'selection', message: `Would you like to update ${pkg.package}?`, choices: ['Major: ' + newMajor, 'Minor: ' + newMinor, 'Patch: ' + patch, 'Custom', 'Skip'] }]).then((selection) => {
+
+            /**If else statement based on the selection by the user */
+            if (selection.selection == 'Skip') {
+                return skip = true;
+            }
+            else if (selection.selection == 'Custom') {
+                return custom = true;
+            }
+            else if (selection.selection == 'Major: ' + newMajor) {
+                pkg.versionNumber = Version.update('major', pkg, this.resetBuildNumber);
+            }
+            else if (selection.selection == 'Minor: ' + newMinor) {
+                pkg.versionNumber = Version.update('minor', pkg, this.resetBuildNumber);
+            }
+            else if (selection.selection == 'Patch: ' + patch) {
+                pkg.versionNumber = Version.update('patch', pkg, this.resetBuildNumber);
+            }
+        });
+        if (skip == true) {
+            return 'skip'
+        }
+        if (custom == true) { return 'custom' }
+    }
+
+    /**
+     * Write the updated config to the sfdx-project.json file
+     */
+    private async writeConfig() {
+        let projectString = JSON.stringify(this.projectConfig, null, 4);
+        if (this.noPrompt) {
+            console.log('Updating sfdx-project.json');
+            fs.writeFileSync('sfdx-project.json', projectString, 'utf-8');
+        }
+        else {
+            await inquirer.prompt([{ type: 'list', name: 'selection', message: `Would you like to write these package updates to the sfdx-project.json?`, choices: ['Yes, Update', 'No, Quit'] }]).then((answer) => {
+                if (answer.selection == 'Yes, Update') {
+                    console.log('Updating sfdx-project.json')
+                    fs.writeFileSync('sfdx-project.json', projectString, 'utf-8');
+                } else {
+                    console.log('Exiting command')
+                    process.exit(1);
+                }
+            });
+        }
+
+    }
+
+
+    /**
+     * Prompt the user on whether or not to update dependent packages
+     * @param pkg 
+     * @param projectConfig 
+     */
+    private async dependentPrompt(pkg) {
+        await inquirer.prompt([{ type: 'list', name: 'selection', message: `Would you like to update packages that are dependent on ${pkg.package}?`, choices: ['Yes', 'No'] }]).then((answer) => {
+            if (answer.selection == 'Yes') {
+                let dependentPkgs = ProjectConfig.getDependents(pkg.package, this.projectConfig);
+                if (dependentPkgs.length != 0) {
+                    dependentPkgs.forEach(dependent => {
+                        dependent = ProjectConfig.updateDependent(pkg, dependent);
+                    });
+
+                }
+            }
+        });
+
+    }
+}
