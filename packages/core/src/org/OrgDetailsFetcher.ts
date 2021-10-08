b606f84a642ac8154b77f4ef1e9a72e081f3cdca
Alan Ly <36793262+aly76@users.noreply.github.com>
2021-10-08 12:19:14 +1100

    Add isSandbox to OrgDetails (#720)
    In a multi-org environment, a production org may not be a Dev Hub, hence we need
    an alternative way to 'isDevHub' for determining whether an org is prod.
    
    Use default directory for aliasfied packages, in deployments to sandboxes only

--- packages/core/src/org/OrgDetailsFetcher.ts
+++ packages/core/src/org/OrgDetailsFetcher.ts
@@ -3,6 +3,7 @@ import extractDomainFromUrl from "../utils/extractDomainFromUrl";
 import { convertAliasToUsername } from "../utils/AliasList";
 import SFPLogger, { LoggerLevel } from "../logger/SFPLogger";
 import ScratchOrgInfoFetcher from "./ScratchOrgInfoFetcher";
+import OrganizationFetcher from "./OrganizationFetcher";
 
 export default class OrgDetailsFetcher {
 
@@ -28,14 +29,16 @@ export default class OrgDetailsFetcher {
       SFPLogger.log(`Unable to get SFDX Auth URL: ${error.message}`, LoggerLevel.TRACE, null);
     }
 
-
     const isScratchOrg = authInfoFields.devHubUsername;
     let scratchOrgInfo = isScratchOrg ? await this.getScratchOrgDetails(authInfoFields.orgId, authInfo) : {} as ScratchOrgDetails;
+
+    const organization = await this.getOrganization(authInfo);
 
     OrgDetailsFetcher.usernamesToOrgDetails[this.username] = {
       sfdxAuthUrl: sfdxAuthUrl,
       ...authInfoFields,
-      ...scratchOrgInfo
+      ...scratchOrgInfo,
+      ...organization
     }
 
     return OrgDetailsFetcher.usernamesToOrgDetails[this.username];
@@ -75,12 +78,34 @@ export default class OrgDetailsFetcher {
       throw new Error(`No information for scratch org with ID ${sfdc.trimTo15(orgId)} found in Dev Hub ${hubOrg.getUsername()}`);
     }
   }
+
+  private async getOrganization(authInfo: AuthInfo) {
+    const connection = await Connection.create({
+      authInfo: authInfo
+    });
+
+    const results = await new OrganizationFetcher(connection).fetch();
+
+    if (results[0]) {
+      return {
+        isSandbox: results[0].IsSandbox,
+        organizationType: results[0].OrganizationType
+      }
+    } else {
+      throw new Error(`No Organization records found for ${connection.getUsername()}`);
+    }
+  }
 }
 
-export interface OrgDetails extends ScratchOrgDetails, AuthFields {
+export interface OrgDetails extends ScratchOrgDetails, AuthFields, Organization {
   sfdxAuthUrl: string;
 };
 
 export interface ScratchOrgDetails {
   status: string;
+}
+
+export interface Organization {
+  isSandbox: boolean;
+  organizationType: string;
 }
