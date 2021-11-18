88b583848d7ec464cb55e8f4be59706deffbff4c
Alan Ly <36793262+aly76@users.noreply.github.com>
2021-11-19 10:02:10 +1100

    feat(package manfiest)!: create package manifest from scratch (#763)
    Create a package manifest from scratch, using a list of components.

--- packages/core/tests/package/PackageManifest.test.ts
+++ packages/core/tests/package/PackageManifest.test.ts
@@ -1,6 +1,7 @@
 import fs from "fs-extra";
 import { jest, expect } from "@jest/globals";
 import PackageManifest from "../../src/package/PackageManifest";
+
 describe("Given a mdapi directory that contains manifest file", () => {
   beforeEach(() => {
     const fsextraMock = jest.spyOn(fs, "readFileSync");
@@ -11,13 +12,197 @@ describe("Given a mdapi directory that contains manifest file", () => {
     );
   });
 
-  it("should  return the manifest in json format", async () => {
+  it("should return the manifest in json format", async () => {
     const packageManifest: PackageManifest = await PackageManifest.create("mdapi");
-    expect(packageManifest.manifest).toStrictEqual(packageManifestJSON);
+    expect(packageManifest.manifestJson).toStrictEqual(packageManifestJSON);
+  });
+
+  it("should return the manifest in xml format", async () => {
+    const packageManifest: PackageManifest = await PackageManifest.create("mdapi");
+    expect(packageManifest.manifestXml).toStrictEqual(packageManifestXML);
+  });
+
+  it("should return true if there is apex", async () => {
+    const packageManifest: PackageManifest = await PackageManifest.create("mdapi");
+    expect(packageManifest.isApexInPackage()).toBe(true);
+  });
+
+  it("should return undefined if there are no triggers", async () => {
+    const packageManifest: PackageManifest = await PackageManifest.create("mdapi");
+    expect(packageManifest.fetchTriggers()).toBe(undefined);
+  });
+
+  it("should return false if there are no profiles", async () => {
+    const packageManifest: PackageManifest = await PackageManifest.create("mdapi");
+    expect(packageManifest.isProfilesInPackage()).toBe(false);
+  });
+
+  it("should return false if there are no permission set groups", async () => {
+    const packageManifest: PackageManifest = await PackageManifest.create("mdapi");
+    expect(packageManifest.isPermissionSetGroupsFoundInPackage()).toBe(false);
   });
 });
 
-let packageManifestJSON = {
+
+describe("Given a list of components", () => {
+  it("should create a package manifest from scratch", () => {
+    const packageManifest: PackageManifest = PackageManifest.createFromScratch(components, "50.0");
+
+    expect(packageManifest).toBeInstanceOf(PackageManifest);
+    expect(packageManifest.manifestJson).toEqual(packageManifestJSON_b);
+    expect(packageManifest.manifestXml).toEqual(packageManifestXML_b);
+  });
+
+  it("should fetch triggers", () => {
+    const packageManifest: PackageManifest = PackageManifest.createFromScratch(components, "50.0");
+
+    expect(packageManifest.fetchTriggers()).toEqual(['ContractTrigger']);
+  });
+
+  it("should return true if there are permission set groups ", () => {
+    const packageManifest: PackageManifest = PackageManifest.createFromScratch(components, "50.0");
+
+    expect(packageManifest.isPermissionSetGroupsFoundInPackage()).toBe(true);
+  });
+
+  it("should return true if there are profiles", () => {
+    const packageManifest: PackageManifest = PackageManifest.createFromScratch(components, "50.0");
+
+    expect(packageManifest.isProfilesInPackage()).toBe(true);
+  });
+
+  it("should return true if there are types supported by profiles", async () => {
+    const packageManifest: PackageManifest = PackageManifest.createFromScratch(components, "50.0");
+    expect(packageManifest.isPayLoadContainTypesSupportedByProfiles()).toBe(true);
+  });
+
+  it("should return true if there are types other than", async () => {
+    const packageManifest: PackageManifest = PackageManifest.createFromScratch(components, "50.0");
+    expect(packageManifest.isPayloadContainTypesOtherThan("Profile")).toBe(true);
+  });
+});
+
+
+describe("Given a manifest json payload", () => {
+  it("should create an instance of PackageManifest", async () => {
+    const packageManifest: PackageManifest = await PackageManifest.createWithJSONManifest(packageManifestJSON);
+
+    expect(packageManifest).toBeInstanceOf(PackageManifest);
+    expect(packageManifest.manifestJson).toEqual(packageManifestJSON);
+    expect(packageManifest.manifestXml).toEqual(packageManifestXML);
+  });
+})
+
+const components: {fullName: string, type: string}[] = [
+  {
+    fullName: "ContractService",
+    type: "ApexClass"
+  },
+  {
+    fullName: "ContractTrigger",
+    type: "ApexTrigger"
+  },
+  {
+    fullName: "ContractVariables__mdt-ContractVariables Layout",
+    type: "Layout"
+  },
+  {
+    fullName: "Contract.Reason__c",
+    type: "CustomField"
+  },
+  {
+    fullName: "Contract.ContractTerm__c",
+    type: "CustomField"
+  },
+  {
+    fullName: "Contract",
+    type: "CustomObject"
+  },
+  {
+    fullName: "ContractPermissionSetGroup",
+    type: "PermissionSetGroup"
+  },
+  {
+    fullName: "Contractor",
+    type: "Profile"
+  }
+];
+
+const packageManifestJSON_b = {
+  Package: {
+    $: {
+      xmlns: "http://soap.sforce.com/2006/04/metadata",
+    },
+    types: [
+      {
+        name: "ApexClass",
+        members: ["ContractService"],
+      },
+      {
+        name: "ApexTrigger",
+        members: ["ContractTrigger"],
+      },
+      {
+        name: "Layout",
+        members: ["ContractVariables__mdt-ContractVariables Layout"],
+      },
+      {
+        name: "CustomField",
+        members: ["Contract.Reason__c", "Contract.ContractTerm__c"],
+      },
+      {
+        name: "CustomObject",
+        members: ["Contract"],
+      },
+      {
+        name: "PermissionSetGroup",
+        members: ["ContractPermissionSetGroup"]
+      },
+      {
+        name: "Profile",
+        members: ["Contractor"]
+      }
+    ],
+    version: "50.0",
+  },
+};
+
+const packageManifestXML_b: string =
+`<?xml version="1.0" encoding="UTF-8"?>
+<Package xmlns="http://soap.sforce.com/2006/04/metadata">
+  <types>
+    <name>ApexClass</name>
+    <members>ContractService</members>
+  </types>
+  <types>
+    <name>ApexTrigger</name>
+    <members>ContractTrigger</members>
+  </types>
+  <types>
+    <name>Layout</name>
+    <members>ContractVariables__mdt-ContractVariables Layout</members>
+  </types>
+  <types>
+    <name>CustomField</name>
+    <members>Contract.Reason__c</members>
+    <members>Contract.ContractTerm__c</members>
+  </types>
+  <types>
+    <name>CustomObject</name>
+    <members>Contract</members>
+  </types>
+  <types>
+    <name>PermissionSetGroup</name>
+    <members>ContractPermissionSetGroup</members>
+  </types>
+  <types>
+    <name>Profile</name>
+    <members>Contractor</members>
+  </types>
+  <version>50.0</version>
+</Package>`;
+
+const packageManifestJSON = {
   Package: {
     $: { xmlns: "http://soap.sforce.com/2006/04/metadata" },
     types: [
@@ -70,8 +255,8 @@ let packageManifestJSON = {
   },
 };
 
-let packageManifestXML: string = `
-<?xml version="1.0" encoding="UTF-8"?>
+const packageManifestXML: string =
+`<?xml version="1.0" encoding="UTF-8"?>
 <Package xmlns="http://soap.sforce.com/2006/04/metadata">
   <types>
     <name>AuraDefinitionBundle</name>
@@ -121,5 +306,4 @@ let packageManifestXML: string = `
     <members>Customer_Fields__mdt.Sobject_Type__c</members>
   </types>
   <version>50.0</version>
-</Package>
-`;
+</Package>`;
