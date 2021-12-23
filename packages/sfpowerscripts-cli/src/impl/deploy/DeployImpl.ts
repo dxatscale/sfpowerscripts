dxatscale/sfpowerscripts

commit 66fe417bf6df1213316749b011c01a76b53c5da0

fix(skip-packages): fix a regression where packages are no longer skipped in release

packages/sfpowerscripts-cli/src/impl/deploy/DeployImpl.ts
@@ -155,7 +155,7 @@ export default class DeployImpl {
                 queue[i].skipTesting,
                 this.props.waitTime.toString(),
                 pkgDescriptor,
-                false
+                this.props.skipIfPackageInstalled
               );
 
 

commit 072937f06ee4909ea9c50eca4155408a4cae8c1c

fix(always-deploy): fix a regression where orchestrator no longer respects alwaysDeploy flag
This PR is to fix a regression caused in
https://github.com/Accenture/sfpowerscripts/commit/66fe417bf6df1213316749b011c01a76b53c5da0, where
orchestrator has already filtered the packages for deployment and it is incorrect to let the
individual implementers to decide skip logic. This probably has to be refactored

packages/sfpowerscripts-cli/src/impl/deploy/DeployImpl.ts
@@ -146,6 +146,7 @@ export default class DeployImpl {
 
               this.displayRetryHeader(this.props.isRetryOnFailure,count);
 
+              
               let installPackageResult = await this.installPackage(
                 packageType,
                 queue[i].package,
@@ -155,7 +156,8 @@ export default class DeployImpl {
                 queue[i].skipTesting,
                 this.props.waitTime.toString(),
                 pkgDescriptor,
-                this.props.skipIfPackageInstalled
+                false                           //Queue already filtered by deploy, there is no further need for individual
+                                                //commands to decide the skip logic. TODO: fix this incorrect pattern
               );
 
 