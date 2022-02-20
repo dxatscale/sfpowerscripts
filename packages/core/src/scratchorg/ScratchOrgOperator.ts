57af1e0dfa05f79150ce652579d7c3094d603f87
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2022-02-20 21:45:30 +1100

    refactor(scratchorg): combine classes into a single operator class responsible for create and delete

--- packages/core/src/scratchorg/ScratchOrgOperator.ts
+++ packages/core/src/scratchorg/ScratchOrgOperator.ts
@@ -4,20 +4,20 @@ import ScratchOrg from "./ScratchOrg";
 import PasswordGenerator from "./PasswordGenerator";
 import SFPLogger, { LoggerLevel } from "../logger/SFPLogger";
 import { Duration } from '@salesforce/kit';
+const retry = require("async-retry");
 
 
-
-export default class CreateScratchOrg {
+export default class ScratchOrgOperator {
   constructor(private hubOrg: Org) {}
 
-  public async createScratchOrg(
-    id: number,
+  public async create(
+   alias:string,
     config_file_path: string,
     expiry: number
   ): Promise<ScratchOrg> {
     SFPLogger.log(
       "Parameters: " +
-        id +
+        alias +
         " " +
         config_file_path +
         " " +
@@ -26,12 +26,12 @@ export default class CreateScratchOrg {
       LoggerLevel.TRACE
     );
 
-    let scatchOrgResult=await this.requestScratchOrg(`SO${id}`,config_file_path,Duration.days(expiry));
+    let scatchOrgResult=await this.requestAScratchOrg(alias,config_file_path,Duration.days(expiry));
     SFPLogger.log(JSON.stringify(scatchOrgResult), LoggerLevel.TRACE);
 
     //create scratchOrg object
     let scratchOrg: ScratchOrg = {
-      alias: `SO${id}`,
+      alias: alias,
       orgId: scatchOrgResult.orgId,
       username: scatchOrgResult.username,
       loginURL:scatchOrgResult.loginURL,
@@ -71,8 +71,18 @@ export default class CreateScratchOrg {
     return scratchOrg;
   }
 
+  public async delete(scratchOrgIds: string[]) {
+    let hubConn = this.hubOrg.getConnection();
+
+    await retry(
+      async (bail) => {
+        await hubConn.sobject("ActiveScratchOrg").del(scratchOrgIds);
+      },
+      { retries: 3, minTimeout: 3000 }
+    );
+  }
   
-  private async requestScratchOrg(alias:string,definitionFile:string,expireIn:Duration)
+  private async requestAScratchOrg(alias:string,definitionFile:string,expireIn:Duration)
   {
     const createCommandOptions: ScratchOrgRequest = {
       durationDays: expireIn.days,
