979734c5396762a3f079b1e6af521969a13ae9a3
aly76 <aly76@users.noreply.github.com>
2022-03-17 23:11:36 +0000

    style(prettier): Apply formatting changes

--- packages/core/src/sfdxwrappers/PushSourceImpl.ts
+++ packages/core/src/sfdxwrappers/PushSourceImpl.ts
@@ -1,7 +1,7 @@
 import { SFDXCommand } from '../command/SFDXCommand';
 
 export default class PushSourceImpl extends SFDXCommand {
-    constructor(protected target_org: string, protected project_directory: string, private waitTime: string = "60") {
+    constructor(protected target_org: string, protected project_directory: string, private waitTime: string = '60') {
         super(target_org, project_directory);
     }
 
