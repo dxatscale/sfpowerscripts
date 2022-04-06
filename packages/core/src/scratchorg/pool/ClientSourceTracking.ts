b7f2db5d47a3d02bb2469521b04ab098adc75279
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2022-04-07 00:43:35 +1000

    perf(source-tracking): add enhancements to increase perfromance of source tracking
    - Ensure artifacts are sorted by the order of the commit id /n - Add addtional logs to display
    progress to user /n - Fix a bug caused by pushing the directory fully

--- packages/core/src/scratchorg/pool/ClientSourceTracking.ts
+++ packages/core/src/scratchorg/pool/ClientSourceTracking.ts
@@ -1,11 +1,14 @@
 const path = require('path');
 import * as fs from 'fs-extra';
-import SFPLogger, { COLOR_KEY_MESSAGE, Logger, LoggerLevel } from '../../logger/SFPLogger';
+import SFPLogger, { COLOR_HEADER, COLOR_KEY_MESSAGE, Logger, LoggerLevel } from '../../logger/SFPLogger';
 import { Connection, SfdxProject } from '@salesforce/core';
 import SFPOrg from '../../org/SFPOrg';
 import { SourceTracking } from '@salesforce/source-tracking';
 import simplegit, { SimpleGit } from 'simple-git';
 import ProjectConfig from '../../project/ProjectConfig';
+import { ComponentSet } from '@salesforce/source-deploy-retrieve';
+import { EOL } from 'os';
+
 const tmp = require('tmp');
 
 export default class ClientSourceTracking {
@@ -49,7 +52,7 @@ export default class ClientSourceTracking {
      * Create local source tracking from sfpowerscripts artifacts installed in scratch org
      */
     private async createLocalSourceTracking() {
-        let tempDir = tmp.dirSync({ unsafeCleanup: true });    
+        let tempDir = tmp.dirSync({ unsafeCleanup: true });
         try {
             let git: SimpleGit = simplegit();
             const repoPath = (await git.getConfig('remote.origin.url')).value;
@@ -67,32 +70,61 @@ export default class ClientSourceTracking {
             tracking.ensureLocalTracking();
 
             git = simplegit(tempDir.name);
+
+            SFPLogger.log(`Total Artifacts to Analyze: ${sfpowerscriptsArtifacts.length}`, LoggerLevel.INFO);
+
+            let count = 1;
             for (const artifact of sfpowerscriptsArtifacts) {
-                SFPLogger.log(`\nAnalyzing package ${COLOR_KEY_MESSAGE(artifact.Name)}`, LoggerLevel.INFO);
+                SFPLogger.log(EOL, LoggerLevel.INFO);
+                SFPLogger.log(COLOR_HEADER(`Package ${count} of ${sfpowerscriptsArtifacts.length}`), LoggerLevel.INFO);
+                SFPLogger.log(`Analyzing package ${COLOR_KEY_MESSAGE(artifact.Name)}`, LoggerLevel.INFO);
                 // Checkout version of source code from which artifact was created
                 await git.checkout(artifact.CommitId__c);
-                SFPLogger.log(`Version pushed while preparing this org ${artifact.Version__c}`, LoggerLevel.INFO);
-                SFPLogger.log(`Checked out SHA ${artifact.CommitId__c}`, LoggerLevel.INFO);
+                SFPLogger.log(
+                    `Version pushed while preparing this org is ${artifact.Version__c} with SHA ${artifact.CommitId__c}`,
+                    LoggerLevel.INFO
+                );
+
                 const projectConfig = ProjectConfig.getSFDXPackageManifest(tempDir.name);
 
                 try {
                     const packageType = ProjectConfig.getPackageType(projectConfig, artifact.Name);
                     if (packageType === 'Unlocked' || packageType === 'Source') {
-                        SFPLogger.log(`Updated tracking for package: ${artifact.Name}`, LoggerLevel.INFO);
-            
+                        let componentSet = ComponentSet.fromSource(
+                            path.join(
+                                tempDir.name,
+                                ProjectConfig.getPackageDescriptorFromConfig(artifact.Name, projectConfig).path
+                            )
+                        );
+                        let components = componentSet.getSourceComponents();
+
+                        //Get all components in the directory
+                        //Count for logging purposes. dont have to waste processing convering
+                        //a lazy collection to array once again
+                        let componentCount = 1;
+                        let componentPaths: string[] = [];
+                        for (const component of components) {
+                            componentCount++;
+                            componentPaths.push(component.xml);
+                            if (component.content) componentPaths.push(component.content);
+                        }
 
                         await tracking.updateLocalTracking({
-                            files: [ProjectConfig.getPackageDescriptorFromConfig(artifact.Name, projectConfig).path],
+                            files: componentPaths,
                         });
-                    }
-                    else
-                      SFPLogger.log(`Data Package Encountered.. skipping`, LoggerLevel.INFO);
+                        SFPLogger.log(
+                            `Updated source tracking for package: ${artifact.Name} with ${componentCount} items`,
+                            LoggerLevel.INFO
+                        );
+                    } else SFPLogger.log(`Encountered data package... skipping`, LoggerLevel.INFO);
                 } catch (error) {
                     SFPLogger.log(`Unable to update local source tracking due to ${error.message}`, LoggerLevel.INFO);
-                    SFPLogger.log(`Skipping package.. ${ artifact.Name }`,LoggerLevel.WARN);
+                    SFPLogger.log(`Skipping package.. ${artifact.Name}`, LoggerLevel.WARN);
                 }
+                count++;
             }
 
+            SFPLogger.log(EOL, LoggerLevel.INFO);
             SFPLogger.log(`Copying the temporary repository over to original location`, LoggerLevel.INFO);
             // Copy source tracking files from temp repo to actual repo
             fs.mkdirpSync(path.join(this.sfdxOrgIdDir, 'localSourceTracking'));
