028556644ac97a6e1b70c1d80dc7bc5e0787a74b
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2022-05-05 12:19:45 +1000

    fix(publish): fix issue when publishing artifacts bigger than 5 MB
    fix #967

--- packages/sfpowerscripts-cli/src/commands/sfpowerscripts/orchestrator/publish.ts
+++ packages/sfpowerscripts-cli/src/commands/sfpowerscripts/orchestrator/publish.ts
@@ -322,7 +322,7 @@ export default class Promote extends SfpowerscriptsCommand {
 
         child_process.execSync(cmd, {
             cwd: artifactRootDirectory,
-            stdio: 'pipe',
+            stdio: 'ignore',
         });
     }
 
