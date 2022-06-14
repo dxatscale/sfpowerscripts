9dccab11ff7b46db89373181c1a7413d29db6b9f
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2022-06-14 14:03:06 +1000

    add exception handling

--- packages/sfpowerscripts-cli/src/impl/validate/ValidateImpl.ts
+++ packages/sfpowerscripts-cli/src/impl/validate/ValidateImpl.ts
@@ -224,9 +224,23 @@ export default class ValidateImpl implements PostDeployHook {
             if (!deploymentResult || deploymentResult.deployed.length == 0) {
                 SFPLogger.log(`Attempting to return scratch org ${scratchOrgUsername} back to pool`, LoggerLevel.INFO);
                 let scratchOrgInfoAssigner = new ScratchOrgInfoAssigner(this.props.hubOrg);
-                let result = await scratchOrgInfoAssigner.setScratchOrgStatus(scratchOrgUsername,'Return');
-                if (result) SFPLogger.log(`Succesfully returned ${scratchOrgUsername} back to pool`, LoggerLevel.INFO);
-                else console.log(COLOR_WARNING(`Unable to update status of scratch org,Please check permissions`));
+                try {
+                    let result = await scratchOrgInfoAssigner.setScratchOrgStatus(scratchOrgUsername, 'Return');
+                    if (result)
+                        SFPLogger.log(`Succesfully returned ${scratchOrgUsername} back to pool`, LoggerLevel.INFO);
+                    else
+                        SFPLogger.log(
+                            COLOR_WARNING(
+                                `Unable to return scratch org to pool,Please check permissions or update sfpower-pool-package to latest`
+                            )
+                        );
+                } catch (error) {
+                    SFPLogger.log(
+                        COLOR_WARNING(
+                            `Unable to return scratch org to pool,Please check permissions or update sfpower-pool-package to latest`
+                        )
+                    );
+                }
             } else {
                 try {
                     if (scratchOrgUsername && this.props.hubOrg.getUsername()) {
