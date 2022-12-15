bf0274e0c2fdfed9ed2f6aeb56f5ce1d11c4a935
Gnemiq <gnemiq@gmail.com>
2022-12-15 15:25:00 +1100

    remove release.yml

--- packages/sfpowerscripts-cli/src/impl/validate/ValidateImpl.ts
+++ packages/sfpowerscripts-cli/src/impl/validate/ValidateImpl.ts
@@ -273,6 +273,7 @@ export default class ValidateImpl implements PostDeployHook, PreDeployHook {
         deploymentResult: DeploymentResult,
         isToDelete: boolean
     ) {
+
         //No scratch org available.. just return
         if (scratchOrgUsername == undefined) return;
 
