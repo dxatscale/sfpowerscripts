008bf1c9bebe1da5ee46ef361676f2ce7b2c81c4
codefactor-io <support@codefactor.io>
2023-01-10 09:08:19 +0000

    [CodeFactor] Apply fixes

--- packages/core/src/utils/RandomId.ts
+++ packages/core/src/utils/RandomId.ts
@@ -1,8 +1,8 @@
 export function makeRandomId(length): string {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
