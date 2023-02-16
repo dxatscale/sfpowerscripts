df9e3c0b1ec94f7f60a58be2dca76c6a6e0795d7
Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>
2023-02-17 06:52:03 +1100

    fix(pools): move recovery functionality to delete (#1237)
    * feat(pool): recover orphaned scratch orgs (#1234)
    
    * feat(pool): recover orphaned scratch orgs
    
    Scratch orgs when created during prepare can be timed out at the client side (aka at the execution
    of prepare), however these scratch orgs might get created in the backend and consume an active
    limit, but is not recovered by sfpowerscripts upon delete. Users have to manually delete thes orgs
    out or wait till these orgs are expired, so that the active limits are back.
     This functionality
    uses description filed to determine which scratch orgs were requested by sfpowerscripts and then
    proceed to delete during an execution of prepare command before and after the pools are created
    
    * [CodeFactor] Apply fixes
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: codefactor-io <support@codefactor.io>
    
    * fix(pool): move recovery of orphaned orgs to delete
    
    recovery of oprhaned scratch orgs are moved to the delete command, with an addition of '-o' flag.
    This will alllow the user to run dur
    
    ---------
    
    Co-authored-by: codefactor-io <support@codefactor.io>

--- packages/core/src/scratchorg/pool/OrphanedOrgsDeleteImpl.ts
+++ packages/core/src/scratchorg/pool/OrphanedOrgsDeleteImpl.ts
@@ -1,46 +1,28 @@
-import SFPLogger from '@dxatscale/sfp-logger';
+import SFPLogger, { Logger, LoggerLevel } from '@dxatscale/sfp-logger';
 import { Org } from '@salesforce/core';
 import { PoolBaseImpl } from './PoolBaseImpl';
 import ScratchOrg from '../ScratchOrg';
 import ScratchOrgInfoFetcher from './services/fetchers/ScratchOrgInfoFetcher';
 import ScratchOrgOperator from '../ScratchOrgOperator';
-import { Logger } from '@dxatscale/sfp-logger';
-import { LoggerLevel } from '@dxatscale/sfp-logger';
 
-export default class PoolDeleteImpl extends PoolBaseImpl {
-    private tag: string;
-    private mypool: boolean;
-    private allScratchOrgs: boolean;
-    private inprogressonly: boolean;
-
-    public constructor(hubOrg: Org, tag: string, mypool: boolean, allScratchOrgs: boolean, inprogressonly: boolean,private logger:Logger) {
+export default class OrphanedOrgsDeleteImpl extends PoolBaseImpl {
+    public constructor(hubOrg: Org, private logger:Logger) {
         super(hubOrg);
         this.hubOrg = hubOrg;
-        this.tag = tag;
-        this.mypool = mypool;
-        this.allScratchOrgs = allScratchOrgs;
-        this.inprogressonly = inprogressonly;
     }
 
     protected async onExec(): Promise<ScratchOrg[]> {
-        const results = (await new ScratchOrgInfoFetcher(this.hubOrg).getScratchOrgsByTag(
-            this.tag,
-            this.mypool,
-            !this.allScratchOrgs
-        )) as any;
+        const results = (await new ScratchOrgInfoFetcher(this.hubOrg).getOrphanedScratchOrgs()) as any;
 
         let scratchOrgToDelete: ScratchOrg[] = new Array<ScratchOrg>();
         if (results.records.length > 0) {
             let scrathOrgIds: string[] = [];
             for (let element of results.records) {
-                if (!this.inprogressonly || element.Allocation_status__c === 'In Progress') {
+                if (element.Description?.includes(`"requestedBy":"sfpowerscripts"`)) {
                     let soDetail: ScratchOrg = {};
                     soDetail.orgId = element.ScratchOrg;
-                    soDetail.loginURL = element.LoginUrl;
                     soDetail.username = element.SignupUsername;
-                    soDetail.expiryDate = element.ExpirationDate;
-                    soDetail.status = 'Deleted';
-
+                    soDetail.status = 'recovered';
                     scratchOrgToDelete.push(soDetail);
                     scrathOrgIds.push(`'${element.Id}'`);
                 }
@@ -54,7 +36,7 @@ export default class PoolDeleteImpl extends PoolBaseImpl {
                 if (activeScrathOrgs.records.length > 0) {
                     for (let scratchOrg of activeScrathOrgs.records) {
                         await new ScratchOrgOperator(this.hubOrg).delete(scratchOrg.Id);
-                        SFPLogger.log(`Scratch org with username ${scratchOrg.SignupUsername} is deleted successfully`,LoggerLevel.TRACE,this.logger);
+                        SFPLogger.log(`Scratch org with username ${scratchOrg.SignupUsername} is recovered`,LoggerLevel.TRACE,this.logger);
                     }
                 }
             }
