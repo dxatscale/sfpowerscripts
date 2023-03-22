844a62bd60125dd2b3da7523cb182fec25834a55
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2023-03-22 12:16:30 +1100

    fix(publish): change flag messages

--- packages/sfpowerscripts-cli/src/commands/sfpowerscripts/orchestrator/publish.ts
+++ packages/sfpowerscripts-cli/src/commands/sfpowerscripts/orchestrator/publish.ts
@@ -445,7 +445,7 @@ export default class Promote extends SfpowerscriptsCommand {
         commitId: string;
     }[], daysToKeep: number
     , limit?: number) {
-        //const pkgs = ProjectConfig.getAllPackages(this.git.getRepositoryPath());
+       
         const tagsToDelete: string[] = [];
 
         await Promise.all(tags.map(async (tag) => {
