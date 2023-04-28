deea70ba9bcb92dcf24e34305678d608c730bcdc
Rocko1204 <ronnyrokitta@web.de>
2023-04-28 13:34:12 +0200

    fix(metrics): new api callout via axios

--- packages/core/src/stats/nativeMetricSenderImpl/SplunkMetricSender.ts
+++ packages/core/src/stats/nativeMetricSenderImpl/SplunkMetricSender.ts
@@ -1,6 +1,6 @@
 import SFPLogger, { Logger, LoggerLevel } from '@dxatscale/sfp-logger';
 import { NativeMetricSender } from '../NativeMetricSender';
-import { Logger as SplunkLogger,SendContextMetadata,SendContext } from 'splunk-logging'
+import axios ,{AxiosInstance} from 'axios';
 
 
 
@@ -9,52 +9,41 @@ export class SplunkMetricSender extends NativeMetricSender {
         super(logger);
     }
 
-    private splunkMetricLogger: SplunkLogger;
-    private payload: SendContextMetadata & SendContext
+    private instance: AxiosInstance;
 
     public initialize(apiHost: string, apiKey: string) {
-        try {
-            this.splunkMetricLogger = new SplunkLogger({ 
-                token: apiKey,
-                url: apiHost, // e.g. https://localhost:8088/services/collector/event
-                maxBatchCount: 0 // Manually flush events
-             });
-        } catch (error) {
-            SFPLogger.log(`Unable to intialize native newrelic metric logger ${error}`, LoggerLevel.WARN, this.logger);
-        }
+          this.instance = axios.create({
+            baseURL: apiHost,
+            headers: {'Authorization': apiKey, 'Content-Type': 'application/json'}
+          });
     }
 
-    public sendGaugeMetric(metric: string, value: number, tags: string[] | { [key: string]: string }) {
+    public async sendGaugeMetric(metric: string, value: number, tags: string[] | { [key: string]: string }) {
         metric = `sfpowerscripts.${metric}`;
-        this.payload = {metadata: {source: "sfpowerscripts",sourcetype: "metrics"}, message: {metric: metric, type: 'guage', value: value,tags: tags as { [key: string]: string }}};
-        this.splunkMetricLogger.send(this.payload);
-        this.splunkMetricLogger.flush((error, response, body) => {
-            if (response) {
-                SFPLogger.log(`Transmitted metric ${metric} ${response.statusCode}`, LoggerLevel.TRACE, this.logger);
-            }
-            if (error)
-                SFPLogger.log(
-                    `Unable to transmit metrics for metric ${metric} due to` + error,
-                    LoggerLevel.WARN,
-                    this.logger
-                );
+        const payload = {source: "sfpowerscripts",sourcetype: "metrics",event: {metric: metric, type: 'guage', value: value,tags: tags as { [key: string]: string },timestamp: Date.now()}};
+        this.instance.post('', JSON.stringify(payload))
+        .then((response) => {SFPLogger.log(`Transmitted metric ${metric} ${response.status}`, LoggerLevel.TRACE, this.logger)})
+        .catch((error) => {
+            SFPLogger.log(
+                `Unable to transmit metrics for metric ${metric} due to` + error,
+                LoggerLevel.WARN,
+                this.logger
+            );
         });
     }
 
     public sendCountMetric(metric: string, tags: string[] | { [key: string]: string }) {
         metric = `sfpowerscripts.${metric}`;
-        this.payload = {metadata: {source: "sfpowerscripts",sourcetype: "metrics"}, message: {metric: metric, type: 'count', tags: tags as { [key: string]: string }}};
-        this.splunkMetricLogger.send(this.payload);
-        this.splunkMetricLogger.flush((error, response, body) => {
-            if (response) {
-                SFPLogger.log(`Transmitted metric ${metric} ${response.statusCode}`, LoggerLevel.TRACE, this.logger);
-            }
-            if (error)
-                SFPLogger.log(
-                    `Unable to transmit metrics for metric ${metric} due to` + error,
-                    LoggerLevel.WARN,
-                    this.logger
-                );
+        const payload = {source: "sfpowerscripts",sourcetype: "metrics",metadata: {metric: metric, type: 'count', tags: tags as { [key: string]: string },timestamp: Date.now()}};
+        this.instance.post('', JSON.stringify(payload))
+        .then((response) => {SFPLogger.log(`Transmitted metric ${metric} ${response.status}`, LoggerLevel.TRACE, this.logger)})
+        .catch((error) => {
+            SFPLogger.log(
+                `Unable to transmit metrics for metric ${metric} due to` + error,
+                LoggerLevel.WARN,
+                this.logger
+            );
         });
     }
 }
+
