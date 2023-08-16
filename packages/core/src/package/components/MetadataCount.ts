1634c1135025e6deb594af5a1a3a82c0ea1cf85c
azlam <azlam@adiza.dev>
2023-08-16 11:13:09 +1000

    fix(metadatacount): fix incorrect usage of new glob lib

--- packages/core/src/package/components/MetadataCount.ts
+++ packages/core/src/package/components/MetadataCount.ts
@@ -1,11 +1,11 @@
-const glob = require('glob')
+import { globSync } from 'glob';
 import path from 'path';
 
 export default class MetadataCount {
     public static async getMetadataCount(projectDirectory: string, sourceDirectory: string): Promise<number> {
         let metadataCount;
         try {
-            let metadataFiles: string[] = await glob(`**/*-meta.xml`, {
+            let metadataFiles: string[] = globSync(`**/*-meta.xml`, {
                 cwd: projectDirectory ? path.join(projectDirectory, sourceDirectory) : sourceDirectory,
                 absolute: true,
             });
