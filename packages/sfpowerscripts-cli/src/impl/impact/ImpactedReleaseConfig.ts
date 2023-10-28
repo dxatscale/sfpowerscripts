ec884dceeb2c4e14b2c346a3f41c85e90667b39d
azlam <azlam@adiza.dev>
2023-10-28 18:32:23 +1100

    feat(impact): add impact:package and impact:releaseconfig commands
    Add two new helper commands that can be used to figure out impacted packages or impacted
    releaseconfigs by comparing against last know gith t  tags

---
+++ packages/sfpowerscripts-cli/src/impl/impact/ImpactedReleaseConfig.ts
@@ -0,0 +1,74 @@
+import * as fs from 'fs-extra';
+const yaml = require('js-yaml');
+import path from 'path';
+
+export default class ImpactedRelaseConfigResolver {
+
+    public getImpactedReleaseConfigs(impactedPackages, configDir, filterBy?: string) {
+        const impactedReleaseDefs = [];
+
+        fs.readdirSync(configDir).forEach((file) => {
+            const filePath = path.join(configDir, file);
+            const fileContent = fs.readFileSync(filePath, 'utf8');
+            const releaseConfig = yaml.load(fileContent);
+
+            if (releaseConfig.releaseName) {
+                let releaseImpactedPackages = [];
+                //Its a releasedefn,
+                if (releaseConfig.includeOnlyArtifacts) {
+                    releaseImpactedPackages = releaseConfig.includeOnlyArtifacts.filter((artifact) =>
+                        impactedPackages.includes(artifact)
+                    );
+                } else if (releaseConfig.excludeArtifacts) {
+                    releaseImpactedPackages = impactedPackages.filter(
+                        (artifact) => !releaseConfig.excludeArtifacts.includes(artifact)
+                    );
+                }
+
+                if (releaseImpactedPackages.length > 0) {
+                    if (filterBy) {
+                        if (releaseConfig.releaseName.includes(filterBy)) {
+                            impactedReleaseDefs.push({
+                                releaseName: releaseConfig.releaseName,
+                                domainNameUsedForPools: releaseConfig.domainNameUsedForPools
+                                    ? releaseConfig.domainNameUsedForPools
+                                    : releaseConfig.releaseName,
+                                filePath: filePath,
+                                impactedPackages: releaseImpactedPackages, // Including the impacted packages
+                            });
+                        }
+                    } else {
+                        impactedReleaseDefs.push({
+                            releaseName: releaseConfig.releaseName,
+                            domainNameUsedForPools: releaseConfig.domainNameUsedForPools
+                                ? releaseConfig.domainNameUsedForPools
+                                : releaseConfig.releaseName,
+                            filePath: filePath,
+                            impactedPackages: releaseImpactedPackages, // Including the impacted packages
+                        });
+                    }
+                }
+            }
+        });
+
+        const sortedImpactedReleaseDefs = impactedReleaseDefs.sort((a, b) => {
+            if (!a.impactedPackages.length && !b.impactedPackages.length) return 0;
+            if (!a.impactedPackages.length) return 1; // Move releases with no impacted packages to the end
+            if (!b.impactedPackages.length) return -1; // Same as above
+
+            const indexA = impactedPackages.indexOf(a.impactedPackages[0]);
+            const indexB = impactedPackages.indexOf(b.impactedPackages[0]);
+
+            if (indexA === -1 && indexB === -1) return 0; // Neither package is in impactedPackages
+            if (indexA === -1) return 1; // Move releases with unknown impacted packages to the end
+            if (indexB === -1) return -1; // Same as above
+
+            return indexA - indexB; // Sort based on index in impactedPackages
+        });
+
+        const output = {
+            include: sortedImpactedReleaseDefs,
+        };
+        return output;
+    }
+}
